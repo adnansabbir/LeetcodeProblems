df048be53ea71430fc46af49d575dc6845c45750
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-01-06 21:56:26 +0600

    Time: 82 ms (28.94%), Space: 14.6 MB (98.45%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -13,11 +13,11 @@ class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         min_d, max_d = self.getMinMaxDistance(trips)
         
-        onboards = [0]*(max_d-min_d+1)
+        onboards = [0]* (max_d + 1)
         total_onboard = 0
         
         for trip in trips:
-            p, f, t = [trip[0], trip[1]-min_d, trip[2]-min_d]
+            p, f, t = trip
             onboards[f]+=p
             onboards[t]-=p
             
