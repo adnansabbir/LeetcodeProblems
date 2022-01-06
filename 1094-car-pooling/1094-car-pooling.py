85d9ee2878e6a54226c47057a2efa6724113c65e
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-01-06 21:37:09 +0600

    Time: 560 ms (5.19%), Space: 14.8 MB (51.55%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,11 +1,22 @@
 class Solution:
-    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        max_dis = max([t[2] for t in trips]) + 1
-        
-        quantity = [0]*max_dis
+    def getMinMaxDistance(self, trips: List[List[int]])-> List[int]:
+        result = [trips[0][1], trips[0][2]]
         
         for trip in trips:
-            p, f, t = trip
+            if result[0]>trip[1]:
+                result[0] = trip[1]
+            if result[1]<trip[2]:
+                result[1] = trip[2]
+        
+        return result
+        
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        min_d, max_d = self.getMinMaxDistance(trips)
+        
+        quantity = [0]*(max_d-min_d)
+        
+        for trip in trips:
+            p, f, t = [trip[0], trip[1]-min_d, trip[2]-min_d]
             
             for i in range(f, t):
                 quantity[i] += p
