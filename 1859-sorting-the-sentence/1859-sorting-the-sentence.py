a731073ebe8df41133b5b1842390299aa3ae0fdb
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-01-14 23:46:18 +0600

    Time: 57 ms (5.06%), Space: 14.3 MB (14.15%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,3 +1,6 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        return ' '.join([word[:-1] for word in sorted(s.split(' '), key= lambda x: int(x[-1]))])
+        words = s.split(' ')
+        words_sorted = sorted(words, key= lambda x: int(x[-1]))
+        words_without_num = [word[:-1] for word in words_sorted]
+        return ' '.join(words_without_num)
