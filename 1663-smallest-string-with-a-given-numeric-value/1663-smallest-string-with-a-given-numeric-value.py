0c9143c2fc82e4d47b4540ced8630680b745ed71
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-03-22 10:19:06 +0600

    Time: 4304 ms (5.53%), Space: 14.9 MB (94.47%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
@@ -1,17 +1,35 @@
 class Solution:
     def getSmallestString(self, n: int, k: int) -> str:
-        result = [1]*n
+        result = ''
         target = k - n
         
         if target == 0:
-            return ''.join(['a']*n)
+            while n:
+                result+='a'
+                n-=1
         
-        pointer = n-1
-        while target:
-            result[pointer] = min(target+1, 26)
-            target -= (result[pointer] - 1)
-            pointer -=1
+        while target >= 25:
+            result = 'z' + result
+            target -= 25
         
-        return ''.join([chr(c + ord('a')-1) for c in result])
+        if target:
+            result = chr(target + ord('a')) + result
+        while len(result) < n:
+            result = 'a' + result
+        # while target:
+        #     if target == 25:
+        #         result = 'z' + result
+        #         target -= 25
+        #     elif target > 25:
+        #         result = 'z' + result
+        #         target -=25
+        #     else:
+        #         result = chr(target + ord('a')) + result
+        #         target = 0
+        
+        # while len(result)<n:
+        #     result = 'a' + result
+            
+        return result
                 
         
