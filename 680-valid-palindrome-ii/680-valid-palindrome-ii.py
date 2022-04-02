8ce736c85657f1cddaad41b733376778f72ba5bb
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-04-02 19:59:14 +0600

    Time: 166 ms (62.31%), Space: 14.5 MB (52.23%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,26 +1,15 @@
 class Solution:
-    def validPalindrome(self, s: str) -> bool:
-        def valid(s: str) -> bool:
-            start, end = 0, len(s) - 1
-
-            canRemove = 1
-            while start<end:
-
-                if s[start] == s[end]:
-                    pass
-                elif canRemove and s[start+1] == s[end]:
-                    start+=1
-                    canRemove -=1
-                elif canRemove and s[start] == s[end-1]:
-                    canRemove -=1
-                    end-=1
-                else:
-                    return False
-
-                start+=1
-                end-=1
-
-            return True
+    def validPalindrome(self, s: str, start = 0, end = None, canRemove = 1) -> bool:
+        if end == None:
+            end = len(s) - 1
         
-        return valid(s) or valid(s[::-1])
+        while start < end:
+            if s[start] != s[end]:
+                canRemove -=1
+                return self.validPalindrome(s, start+1, end, canRemove) or self.validPalindrome(s, start, end-1, canRemove) if canRemove+1 else False
+            
+            start +=1
+            end -=1
+        
+        return True
         
