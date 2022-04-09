98e498e085d00c03d76bb8aba063850b409e7608
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-04-09 22:32:04 +0600

    Time: 133 ms (58.58%), Space: 18.7 MB (40.43%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,33 @@
+import heapq
+
+class Node(object):
+    def __init__(self, val: int, freq: int):
+        self.val = val
+        self.freq = -freq
+    
+    def __repr__(self):
+        return f'{self.val}-{self.freq}'
+    
+    def __lt__(self, other):
+        return self.freq < other.freq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        frequency = collections.defaultdict(int)
+        uniqNums = set()
+        
+        for num in nums:
+            frequency[num]+=1
+            uniqNums.add(num)
+        
+        heapedNumNodes = [Node(num, frequency[num]) for num in uniqNums]
+        heapq.heapify(heapedNumNodes)
+        
+        result = []
+        while k:
+            result.append(heapq.heappop(heapedNumNodes).val)
+            k -=1
+        
+        return result
+            
+        
