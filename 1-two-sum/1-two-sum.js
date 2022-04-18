2b041098aba2f55c5afbad0a094410cffa58946b
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-04-18 21:37:39 +0600

    Time: 63 ms (96.58%), Space: 42.7 MB (47.07%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const idx_map = {}
+    
+    for (let i=0; i<nums.length; i++){
+        if(idx_map[target-nums[i]] !== undefined){
+            return [idx_map[target-nums[i]], i]
+        }
+        else{
+            idx_map[nums[i]] = i
+        }
+    }
+};
