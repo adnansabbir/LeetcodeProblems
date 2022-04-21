bf57201297d5cb0c416fc4006d3cc8210f162fa0
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-04-21 23:04:48 +0600

    Time: 211 ms (68.03%), Space: 57.5 MB (19.26%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.js
@@ -0,0 +1,46 @@
+class MyHashSet{
+    constructor(key){
+        this.size = Math.pow(10, 6) + 1;
+        this.data = [];
+    }
+    
+    _getHashKey(key){
+        return key % this.size;
+    }
+    
+    add(key){
+        const hashKey = this._getHashKey(key)
+        const bucket = this.data[hashKey]
+        if(bucket === undefined){
+            this.data[hashKey] = [key]
+            return
+        }
+        
+        if(bucket.indexOf(key) !== -1) return
+        
+        bucket.push(key)
+    }
+    
+    remove(key){
+        const hashKey = this._getHashKey(key)
+        const bucket = this.data[hashKey]
+        if(bucket === undefined || bucket.indexOf(key) === -1){
+            return
+        }
+        
+        bucket.pop(bucket.indexOf(key))
+        if(!bucket.length){
+            this.data[hashKey] = undefined
+        }
+    }
+    
+    contains(key){
+        const hashKey = this._getHashKey(key)
+        const bucket = this.data[hashKey]
+        if(bucket === undefined){
+            return false
+        }
+        
+        return bucket.indexOf(key) !== -1
+    }
+}
