70a814479cd6f983247033b8c9b876d6713c127b
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-04-23 04:52:11 +0600

    Time: 605 ms (81.29%), Space: 101.7 MB (74.51%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.js
@@ -0,0 +1,114 @@
+class DLL{
+    constructor(val = null, key = null, prev = null, next = null){
+        this.val = val
+        this.prev = prev
+        this.next = next
+        this.key = key
+    }
+}
+
+class LRUCache{
+    constructor(capacity){
+        this.capacity = capacity
+        this.size = 0
+        
+        this.head = new DLL()
+        this.tail = new DLL(null, null, this.head)
+        this.head.next = this.tail
+        
+        this.ref = {}
+    }
+    
+    get(key){
+        if(this.ref[key] === undefined) return -1
+        
+        const node = this._removeNode(this.ref[key])
+        this._addNodeToEnd(node)
+        
+        return node.val
+    }
+    
+    put(key, value){
+        if(this.ref[key] === undefined){
+            // add new node to the end
+            // add new ref
+            const newNode = new DLL(value, key)
+            this._addNodeToEnd(newNode)
+            this.ref[key] = newNode
+            this.size++
+            
+            // check size
+            // if size is full -> remove first node and it's ref
+            if(this.size > this.capacity){
+                this.size--
+                const removedNode = this._removeNode(this.head.next)
+                this.ref[removedNode.key] = undefined
+            }
+            
+            return
+        }
+        
+        // if key is already at the end, update value and return
+        if(this.tail.prev.key === key){
+            this.tail.prev.val = value
+            return
+        }else{
+            // move the node to end and update value
+            const node = this._removeNode(this.ref[key])
+            node.val = value
+            this._addNodeToEnd(node)
+            return
+        }
+        
+    }
+    
+    _removeNode(node){
+        if(!node) return
+        const prevNode = node.prev
+        const nextNode = node.next
+        
+        prevNode.next = nextNode
+        nextNode.prev = prevNode
+        
+        return node
+    }
+    
+    _addNodeToEnd(node){
+        const lastNode = this.tail.prev
+        lastNode.next = node
+        node.next = this.tail
+        node.prev = lastNode
+        this.tail.prev = node
+    }
+}
+
+// /**
+//  * @param {number} capacity
+//  */
+// var LRUCache = function(capacity) {
+    
+// };
+
+// /** 
+//  * @param {number} key
+//  * @return {number}
+//  */
+// LRUCache.prototype.get = function(key) {
+    
+// };
+
+// /** 
+//  * @param {number} key 
+//  * @param {number} value
+//  * @return {void}
+//  */
+// LRUCache.prototype.put = function(key, value) {
+    
+// };
+
+// /** 
+//  * Your LRUCache object will be instantiated and called as such:
+//  * var obj = new LRUCache(capacity)
+//  * var param_1 = obj.get(key)
+//  * obj.put(key,value)
+//  */
