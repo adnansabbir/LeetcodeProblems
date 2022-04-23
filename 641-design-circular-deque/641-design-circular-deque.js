8937841eeb855aa8006b85686aa92ea823223211
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-04-23 06:44:22 +0600

    Time: 119 ms (83.64%), Space: 50.2 MB (78.18%) - LeetHub

--- 641-design-circular-deque/641-design-circular-deque.js
+++ 641-design-circular-deque/641-design-circular-deque.js
@@ -8,20 +8,6 @@ class MyCircularDeque{
         this.end = k - 1
     }
     
-//     _setFirstValue(value){
-//         this.start = 0
-//         this.end = 0
-//         this.values[0] = value
-//         this.length++
-//     }
-    
-//     _deleteLastValue(){
-//         this.values[this.start] = null
-//         this.length--
-//         this.start = null
-//         this.end = null
-//     }
-    
     _resetPosIfEmpty(){
         if(this.isEmpty()){
             this.start = 0
@@ -95,80 +81,3 @@ class MyCircularDeque{
         return this.length === this.size
     }
 }
-// /**
-//  * @param {number} k
-//  */
-// var MyCircularDeque = function(k) {
-    
-// };
-
-// /** 
-//  * @param {number} value
-//  * @return {boolean}
-//  */
-// MyCircularDeque.prototype.insertFront = function(value) {
-    
-// };
-
-// /** 
-//  * @param {number} value
-//  * @return {boolean}
-//  */
-// MyCircularDeque.prototype.insertLast = function(value) {
-    
-// };
-
-// /**
-//  * @return {boolean}
-//  */
-// MyCircularDeque.prototype.deleteFront = function() {
-    
-// };
-
-// /**
-//  * @return {boolean}
-//  */
-// MyCircularDeque.prototype.deleteLast = function() {
-    
-// };
-
-// /**
-//  * @return {number}
-//  */
-// MyCircularDeque.prototype.getFront = function() {
-    
-// };
-
-// /**
-//  * @return {number}
-//  */
-// MyCircularDeque.prototype.getRear = function() {
-    
-// };
-
-// /**
-//  * @return {boolean}
-//  */
-// MyCircularDeque.prototype.isEmpty = function() {
-    
-// };
-
-// /**
-//  * @return {boolean}
-//  */
-// MyCircularDeque.prototype.isFull = function() {
-    
-// };
-
-// /** 
-//  * Your MyCircularDeque object will be instantiated and called as such:
-//  * var obj = new MyCircularDeque(k)
-//  * var param_1 = obj.insertFront(value)
-//  * var param_2 = obj.insertLast(value)
-//  * var param_3 = obj.deleteFront()
-//  * var param_4 = obj.deleteLast()
-//  * var param_5 = obj.getFront()
-//  * var param_6 = obj.getRear()
-//  * var param_7 = obj.isEmpty()
-//  * var param_8 = obj.isFull()
-//  */
