eddd832fd04d10b003d53cc5fd5f156de5436f1c
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-04-24 02:15:17 +0600

    Time: 111 ms (29.81%), Space: 43.9 MB (50.96%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.js
@@ -0,0 +1,49 @@
+baseUrl = "http://tinyurl.com/"
+hashSize = 7
+map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+encodedURL = {}
+decodedURL = {}
+
+
+const _generateRandomHash = () => {
+    let newHash = ""
+    for (let i = 0; i < hashSize; i++){
+        const randomIdx = Math.floor(Math.random() * map.length)
+        newHash += map[randomIdx]
+    }
+
+    return encodedURL[newHash] === undefined ? newHash : _generateRandomHash()
+}
+// /**
+//  * Encodes a URL to a shortened URL.
+//  *
+//  * @param {string} longUrl
+//  * @return {string}
+//  */
+var encode = function(longUrl) {
+    if(encodedURL[longUrl] !== undefined){
+      return baseUrl+encodedURL[longUrl]
+    }  
+    
+    const newHash = _generateRandomHash()
+    encodedURL[longUrl] = newHash
+    decodedURL[newHash] = longUrl
+    
+    return baseUrl + newHash
+};
+
+// /**
+//  * Decodes a shortened URL to its original URL.
+//  *
+//  * @param {string} shortUrl
+//  * @return {string}
+//  */
+var decode = function(shortUrl) {
+    const hashKey = shortUrl.substring(baseUrl.length)
+    return decodedURL[hashKey] || null
+};
+
+// /**
+//  * Your functions will be called as such:
+//  * decode(encode(url));
+//  */
