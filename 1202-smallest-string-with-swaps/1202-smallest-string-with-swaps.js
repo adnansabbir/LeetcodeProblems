e9be77b5aa4f6ed74ecb4d3f87b1b89de23075e3
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-04-28 03:29:48 +0600

    Time: 201 ms (95.41%), Space: 70.6 MB (93.58%) - LeetHub

---
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.js
@@ -0,0 +1,62 @@
+class DisjointSet{
+	constructor(size){
+		this.parents = Array(size).fill(0).map((_,i)=> i)
+	}
+	
+	find(i){
+		this.parents[i] = this.parents[i] !== i ? this.find(this.parents[i]) : this.parents[i]
+		return this.parents[i]
+	}
+	
+	union(x, y){
+		const xSet = this.find(x)
+		const ySet = this.find(y)
+		if(xSet === ySet) return
+		
+		this.parents[ySet] = xSet
+	}
+	
+	getGroups(){
+		this.parents.forEach((p, idx) => this.find(idx))
+		
+		const groups = {}
+		this.parents
+		.forEach((p, idx) => {
+			if(p === idx) return
+			
+			if(groups[p] === undefined){
+				groups[p] = [p]
+			}
+			groups[p].push(idx)
+		})
+		
+		return groups
+	}
+
+}
+
+const sortChars = (sArr, group) => {
+	const selected = group.map(idx => sArr[idx])
+	selected.sort()
+	selected.forEach((val, idx) => sArr[group[idx]] = val)
+}
+
+const smallestStringWithSwaps = (s, pairs) =>  {
+    const disjointSet = new DisjointSet(s.length)
+	// console.log(disjointSet.parents)
+	pairs.forEach(pair => {
+		disjointSet.union(pair[0], pair[1])
+		// console.log(disjointSet.parents)
+	})
+	
+	const stringArr = s.split('')
+	const groups = disjointSet.getGroups()
+	// console.log('\n\n', disjointSet.parents)
+	Object.keys(groups).forEach(k => {
+		groups[k].sort((a, b) => a-b)
+		// console.log(groups[k])
+		sortChars(stringArr, groups[k])
+	})
+	
+	return stringArr.join('')
+};
