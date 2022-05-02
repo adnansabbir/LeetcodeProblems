26608a071ee4c4297392e33ec26953599beb33aa
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-05-02 18:45:39 +0600

    Time: 85 ms (74.10%), Space: 44.7 MB (50.08%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.js
+++ 905-sort-array-by-parity/905-sort-array-by-parity.js
@@ -3,6 +3,27 @@
  * @return {number[]}
  */
 var sortArrayByParity = function(nums) {
-    nums.sort((a, b) => a%2 - b%2)
+    let start = 0
+    let end = nums.length - 1
+    
+    while(start < end){
+        
+        if(nums[start] % 2 === 0){
+            start++
+        }
+        
+        if(nums[end] % 2 !== 0){
+            end--
+        }
+        
+        if(nums[start] % 2 !== 0 && nums[end] % 2 === 0 && start < end){
+            const temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp
+            start++
+            end--
+        }
+    }
+    
     return nums
 };
