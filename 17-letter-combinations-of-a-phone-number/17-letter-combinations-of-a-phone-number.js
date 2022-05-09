ef3caa69854676bd51c981f982b8d1f874a05e7a
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-05-09 19:23:43 +0600

    Time: 74 ms (54.01%), Space: 41.9 MB (70.79%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -4,28 +4,28 @@
  */
 
 const _map = {
-        2: 'abc',
-        3: 'def',
-        4: 'ghi',
-        5: 'jkl',
-        6: 'mno',
-        7: 'pqrs',
-        8: 'tuv',
-        9: 'wxyz'
+        2: ['a','b','c'],
+        3: ['d','e','f'],
+        4: ['g','h','i'],
+        5: ['j','k','l'],
+        6: ['m','n','o'],
+        7: ['p','q','r','s'],
+        8: ['t','u','v'],
+        9: ['w','x','y','z']
     }
 
 var letterCombinations = function(digits) {
-
     if(!digits.length) return []
-    if(digits.length === 1) return _map[digits].split('')
-
-    const chars = _map[digits.charAt(0)].split('')
-    const subRes = letterCombinations(digits.substring(1))
-    const result = []
-    subRes.forEach(sr => {
-        chars.forEach(c => result.push(c+sr))
-    })
-    return result
+    const stack = [..._map[digits[0]]]
     
-    return result
+    while(stack[0].length < digits.length){
+        const size = stack.length
+        const pointer = stack[0].length
+        for(let i = 0; i< size; i++){
+            const word = stack.shift()
+            _map[digits[pointer]].forEach(c => stack.push(word+c))
+        }
+    }
+    
+    return stack
 };
