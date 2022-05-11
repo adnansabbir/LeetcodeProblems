bca18fa597cb067700e66deb1741b4fce799eb7c
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-05-11 19:27:16 +0600

    Time: 89 ms (53.38%), Space: 42.2 MB (47.30%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
@@ -3,14 +3,22 @@
  * @return {number}
  */
 
-const backTrack = (count, idx, n) => {
-    if(count === n) return 1
-    if(idx === 5) return 0
-    return backTrack(count+1, idx, n) + backTrack(count, idx + 1, n)
-}
-
 var countVowelStrings = function(n) {
     if(n === 1) return 5
+    const dp = new Array(n+1).fill(0).map(() => new Array(5).fill(0))
+    for(let i = 0; i < 5; i++){
+        dp[1][i] = 1
+    }
     
-    return backTrack(0, 0, n)
+    let sum = 5
+    
+    for(let i = 2; i < dp.length; i++){
+        dp[i][0] = sum
+        for(let j = 1; j < 5; j++){
+            dp[i][j] = dp[i][j-1] - dp[i-1][j-1]
+            sum += dp[i][j]
+        }
+    }
+    
+    return sum
 };
