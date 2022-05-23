e630d8fce38250faf754246e48aa15f1d324650b
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-05-23 23:19:07 +0600

    Time: 256 ms (66.28%), Space: 44.3 MB (76.74%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.js
+++ 474-ones-and-zeroes/474-ones-and-zeroes.js
@@ -18,24 +18,17 @@ var findMaxForm = function(strs, m, n) {
         return result
     }
     
-    const dp = new Array(m+1).fill(0).map(c => new Array(n+1).fill(0).map(d=> new Array(strs.length).fill(0)))
-    // console.log(dp)
-    const take = (idx, zeros, ones) => {
-        if(dp[zeros][ones][idx] > 0) return dp[zeros][ones][idx]
-        if(idx === strs.length || zeros + ones === 0) return 0
-        const count = freq(idx)
-        
-        // consider current one
-        let consider = 0
-        if(zeros >= count[0] && ones >= count[1]){
-            consider = 1 + take(idx + 1, zeros - count[0], ones - count[1])
+    const dp = new Array(m+1).fill(0).map(c => new Array(n+1).fill(0))
+    
+    for(let i = 0; i < strs.length; i++){
+        const count = freq(i)
+        for(let zeros = m; zeros >= count[0]; zeros--){
+            for(let ones = n; ones >= count[1]; ones--){
+                dp[zeros][ones] = Math.max(1 + dp[zeros - count[0]][ones - count[1]], dp[zeros][ones])
+            }   
         }
-        
-        const skip = take(idx + 1, zeros, ones)
-        
-        dp[zeros][ones][idx] = Math.max(consider, skip)
-        return dp[zeros][ones][idx]
     }
     
-    return take(0, m, n)
+    return dp[m][n]
+    
 };
