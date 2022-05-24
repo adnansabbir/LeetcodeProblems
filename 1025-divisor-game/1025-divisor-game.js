487cce1ee07e7bb2e18c16c4c99f936b09a39073
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-05-24 22:56:31 +0600

    Time: 96 ms (11.36%), Space: 42.5 MB (8.44%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.js
+++ 1025-divisor-game/1025-divisor-game.js
@@ -2,11 +2,11 @@
  * @param {number} n
  * @return {boolean}
  */
-var divisorGame = function(n, dp = {1: false}) {
+var divisorGame = function(n, dp = {1: false, 0: true}) {
     if(dp[n] !== undefined) return dp[n]
     
-    for(let i = 1; i < n; i++){
-        if(n % i === 0 && divisorGame(n-i, dp) === false){
+    for(let i = 1; i * i <= n; i++){
+        if(n % i === 0 && (divisorGame(n-i, dp) === false || divisorGame(n - (n / i), dp) === false)){
             dp[n] = true
             return true
         }
