1a22a77a35ffb6cfbcea08882d8528428464fd86
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-05-26 11:26:51 +0600

    Time: 101 ms (30.99%), Space: 42.6 MB (47.77%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let count = 0
+    while(n){
+        count += n & 1
+        n = n >>> 1
+    }
+    
+    return count
+};
