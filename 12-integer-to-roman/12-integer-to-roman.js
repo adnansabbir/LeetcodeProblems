313d49e46d2ff69beb3671498edde2aca835f98c
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-05-31 21:31:09 +0600

    Time: 295 ms (7.64%), Space: 49.1 MB (27.42%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.js
+++ 12-integer-to-roman/12-integer-to-roman.js
@@ -5,35 +5,33 @@
 var intToRoman = function(num) {
     const roman = {
         1: "I",
+        4: "IV",
         5: "V",
+        9: "IX",
         10: "X",
+        40: "XL",
         50: "L",
+        90: "XC",
         100: "C",
+        400: "CD",
         500: "D",
+        900: "CM",
         1000: "M",
     }
     
-    const sequence = [1000,500,100,50,10,5,1,0]
+    const seq = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
     
-    const generateRoman = (n, idx = 0) => {
-        if(!n) return ""
-        const number = sequence[idx]
-        const nextNumber = number / 2 === sequence[idx+1] ? sequence[idx+2] : sequence[idx+1]
-        let result = ""
-        if(n >= number){
-            result += new Array(parseInt(n / number)).fill(roman[number]).join('')
-            result += generateRoman(n % number, idx)
-            
-            
-        }else if(n >= number - nextNumber){
-            result += new Array(parseInt(n / (number - nextNumber))).fill(`${roman[nextNumber]}${roman[number]}`).join('')
-            result += generateRoman(n % (number - nextNumber), idx + 1)
-        }else{
-            result += generateRoman(n, idx + 1)
+    let result = ""
+    let i = 0
+    while(num){
+        if(num >= seq[i]){
+            result += roman[seq[i]]
+            num -= seq[i]
+            continue
         }
-
-        return result 
+        
+        i++
     }
     
-    return generateRoman(num)
+    return result
 };
