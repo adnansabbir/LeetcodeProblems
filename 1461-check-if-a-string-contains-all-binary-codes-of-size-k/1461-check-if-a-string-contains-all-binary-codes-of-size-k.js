dc0f784a370e6d22ba5c931971bd85e07c6306af
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-05-31 10:56:54 +0600

    Time: 426 ms (33.33%), Space: 66.9 MB (22.22%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {boolean}
+ */
+
+var hasAllCodes = function(s, k) {
+    if(s.length < k + 1) return false
+    const availableNums = new Set()
+    for(let i = k; i <= s.length; i++){
+        availableNums.add(parseInt(s.substring(i-k, i), 2))
+    }
+    return availableNums.size === Math.pow(2, k)
+};
