569443f79a0963c3a53b2ef9072d552f33bb99eb
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-06-08 21:46:20 +0600

    Time: 79 ms (57.61%), Space: 42 MB (44.57%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.js
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.js
@@ -2,6 +2,13 @@
  * @param {string} s
  * @return {number}
  */
+
 var removePalindromeSub = function(s) {
-    return !s ? 0 : s === s.split('').reverse().join('') ? 1 : 2
+    if(!s) return 0
+    
+    for(let i = 0; i < s.length; i++){
+        if(s[i] !== s[s.length - 1 - i]) return 2
+    }
+    
+    return 1
 };
