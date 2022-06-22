8346e7b6ec97ed103851457dcf6979977acdcf7e
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-06-22 15:56:28 +0600

    Time: 140 ms (28.14%), Space: 44.9 MB (89.62%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.js
+++ 227-basic-calculator-ii/227-basic-calculator-ii.js
@@ -3,59 +3,45 @@
  * @return {number}
  */
 
-class Node{
-    constructor(val, next){
-        this.val = val
-        this.next = next
-    }
-}
-
 const isDigit = (c) => {
     return c.charCodeAt(0) >= 48 && c.charCodeAt(0) < 58
 }
 
 var calculate = function(s) {
-    const headNode = new Node()
-    let temp = ""
+    const stack = []
+    let currentNum = ""
+    let operation = ""
     
-    let tempHead = headNode
     for(let i = 0; i < s.length; i++){
         if(isDigit(s[i])){
-            temp += s[i]
-        }else if(['+', '-', '*', '/'].includes(s[i])){
-            tempHead.next = new Node(parseInt(temp), new Node(s[i]))
-            tempHead = tempHead.next.next
-            temp = ""
-        }
-    }
-    tempHead.next = new Node(parseInt(temp))
-    
-    tempHead = headNode.next
-    while(tempHead && tempHead.next){
-        const expNode = tempHead.next
-        if(expNode.val === '*'){
-            tempHead.val = tempHead.val * tempHead.next.next.val
-            tempHead.next = tempHead.next.next.next
-        }else if(expNode.val === '/'){
-            tempHead.val = parseInt(tempHead.val / tempHead.next.next.val)
-            tempHead.next = tempHead.next.next.next
-        }else{
-            tempHead = tempHead.next.next    
+            currentNum += s[i]
         }
         
-    }
-    
-    tempHead = headNode.next
-    while(tempHead && tempHead.next){
-        const expNode = tempHead.next
-        if(expNode.val === '+'){
-            tempHead.val = tempHead.val + tempHead.next.next.val
-            tempHead.next = tempHead.next.next.next
-        }else if(expNode.val === '-'){
-            tempHead.val = tempHead.val - tempHead.next.next.val
-            tempHead.next = tempHead.next.next.next
+        if((!isDigit(s[i]) && s[i] !== ' ') || i === s.length - 1){
+            switch(operation){
+                case '+':
+                    stack.push(parseInt(currentNum))
+                    break
+                case '-':
+                    stack.push(parseInt(currentNum) * -1)
+                    break
+                case '*':
+                    stack.push(stack.pop() * parseInt(currentNum))
+                    break
+                case '/':
+                    stack.push(parseInt(stack.pop() / parseInt(currentNum)))
+                    break
+                default:
+                    stack.push(parseInt(currentNum))
+            }
+            operation = s[i]
+            currentNum = ""
         }
     }
     
-    return headNode.next.val
+    let result = 0
+    while(stack.length){
+        result += stack.pop()
+    }
+    return result
 };
