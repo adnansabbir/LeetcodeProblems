7923bccb6751902256f4131d29b23978c8c7d0e0
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-06-23 21:15:10 +0600

    Time: 226 ms (9.61%), Space: 48.9 MB (70.24%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -4,18 +4,17 @@
  */
 var merge = function(intervals) {
     intervals.sort((a, b)=> a[0] - b[0])
-    console.log(intervals)
+    let result = [intervals[0]]
+    
     for(let i = 1; i < intervals.length; i++){
-        if(intervals[i][0] > intervals[i-1][1]){
+        if(intervals[i][0] > result.at(-1)[1]){
+            result.push(intervals[i])
             continue
         }
         
-        if(intervals[i][1] > intervals[i-1][1]){
-           intervals[i-1][1] = intervals[i][1]
-        }
-        intervals.splice(i,1)
-        i--
+        if(intervals[i][1] <= result.at(-1)[1]) continue
+        result.push([result.pop()[0], intervals[i][1]])
     }
     
-    return intervals
+    return result
 };
