14e8b43a93e542978887919080ae257ff7477568
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-06-23 21:50:19 +0600

    Time: 90 ms (86.07%), Space: 44.7 MB (78.12%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -4,24 +4,35 @@
  */
 
 var setZeroes = function(matrix) {
-    let row = new Set()
-    let col = new Set()
-    
+    let isCol = false
     for(let i = 0; i < matrix.length; i++){
-        for(let j = 0; j < matrix[0].length; j++){
+        if(!matrix[i][0]) isCol = true
+        
+        for(let j = 1; j < matrix[0].length; j++){
             if(!matrix[i][j]){
-                row.add(i)
-                col.add(j)
+                matrix[i][0] = 0
+                matrix[0][j] = 0
             }
         }
     }
     
-    
-    for(let i = 0; i < matrix.length; i++){
-        for(let j = 0; j < matrix[0].length; j++){
-            if(row.has(i) || col.has(j)){
+    for(let i = 1; i < matrix.length; i++){
+        for(let j = 1; j < matrix[0].length; j++){
+            if(!matrix[i][0] || !matrix[0][j]){
                 matrix[i][j] = 0
             }
+        }
+    }
+    
+    if(!matrix[0][0]){
+        for(let j = 0; j < matrix[0].length; j++){
+            matrix[0][j] = 0
+        }
+    }
+    
+    if(isCol){
+        for(let i = 0; i < matrix.length; i++){
+            matrix[i][0] = 0
         }
     }
     
