972a7f4c7d5c594978f93864bf5a20b903316eb1
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-06-28 09:23:19 +0600

    Time: 357 ms (29.32%), Space: 14.9 MB (51.90%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -12,16 +12,13 @@ class Solution:
             if not freq:
                 continue
             
-            # print(chr(i+97), freq, freq not in freqSet)
             if freq not in freqSet:
                 freqSet.add(freq)
             else:
                 for j in range(freq - 1, -1, -1):
                     result += 1
-                    # print('\t', chr(i+97), j, j not in freqSet, result)
                     if j not in freqSet and i > 0:
                         freqSet.add(j)
                         break
         
-        # print(freqSet)
         return result
