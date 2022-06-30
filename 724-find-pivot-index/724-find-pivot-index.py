b9030ff966058cc93db21970dcc463f499fa6cfd
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-06-30 11:49:09 +0600

    Time: 183 ms (76.83%), Space: 15.4 MB (10.73%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -0,0 +1,19 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        sum1 = [nums[0]]
+        sum2 = [nums[-1]]
+        
+        for num in nums[1:]:
+            sum1.append(sum1[-1] + num)
+            
+        for num in reversed(nums[:-1]):
+            sum2.append(sum2[-1] + num)
+        
+        print(sum1, sum2)
+        
+        for i in range(0, len(nums)):
+            if sum1[i] == sum2[len(nums) - i - 1]:
+                return i
+        
+        return -1
+        
