cbaf50bda54b20e21a4be1254259a54c67f79726
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-07-06 11:19:15 +0600

    Time: 78 ms (28.90%), Space: 13.8 MB (54.45%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,10 @@
+from functools import lru_cache
+
+class Solution:
+    @lru_cache(maxsize=None)
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        
+        return self.fib(n-1) + self.fib(n-2)
+        
