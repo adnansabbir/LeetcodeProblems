3c7aee030ef469a9bf0a61658113915abb2cb9d8
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-07-10 23:05:01 +0600

    Time: 107 ms (28.58%), Space: 14.1 MB (44.40%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,12 +1,10 @@
-from functools import lru_cache
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        @lru_cache(maxsize=None)
-        def climb(stair):
-            if stair >= len(cost):
-                return 0
-            
-            return cost[stair] + min(climb(stair + 1), climb(stair + 2))
+        cost.append(0)
         
-        return min(climb(0), climb(1))
+        for i in reversed(range(len(cost)-3)):
+            cost[i] += min(cost[i+1], cost[i+2])
+        
+        return min(cost[0], cost[1])
+            
         
