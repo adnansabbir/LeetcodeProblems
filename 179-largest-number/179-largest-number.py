4fbc792431b6402841fee60832d33c30107f6cc6
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-07-14 22:21:42 +0600

    Time: 45 ms (86.88%), Space: 14 MB (21.16%) - LeetHub

--- 179-largest-number/179-largest-number.py
+++ 179-largest-number/179-largest-number.py
@@ -5,13 +5,6 @@ class Solution:
             key2 = f"{y}{x}"
             return 1 if key1 < key2 else -1
             
-#         arr = list(map(lambda x: (str(x), str(x).ljust(10, str(x)[-1])), nums))
-#         arr.sort(key=lambda x: x[1], reverse=True)
-        
-#         print(arr)
-#         result = ""
-#         for val,_ in arr:
-#             result +=val
         nums.sort(key=functools.cmp_to_key(compare))
         return ''.join([str(num) for num in nums]).lstrip('0') or '0'
         
