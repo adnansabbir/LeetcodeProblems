8899f42053ed53cfd9f3f4ab175c2692eb97386c
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-07-15 16:06:00 +0600

    Time: 131 ms (97.00%), Space: 16.6 MB (58.06%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,18 +1,16 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        explored = set()
         max_area = 0
         def explore(x,y)-> int:
-            key = f"{x}-{y}"
-            if (0 > x or x >= len(grid)) or (0 > y or y >= len(grid[0])) or (key in explored) or not grid[x][y]:
+            if (0 > x or x >= len(grid)) or (0 > y or y >= len(grid[0])) or not grid[x][y]:
                 return 0
             
-            explored.add(key)
+            grid[x][y] = 0
             return 1 + explore(x-1, y) + explore(x+1, y) + explore(x, y-1) + explore(x, y+1)
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] and f'{i}-{j}' not in explored:
+                if grid[i][j]:
                     max_area = max(max_area, explore(i,j))
         
         return max_area
