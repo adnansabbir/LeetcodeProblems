6c9d9ea6e61562f891b69a9c755cf78c058bf709
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-07-15 16:19:23 +0600

    Time: 170 ms (77.07%), Space: 14.1 MB (92.54%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -2,11 +2,31 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         max_area = 0
         def explore(x,y)-> int:
-            if (0 > x or x >= len(grid)) or (0 > y or y >= len(grid[0])) or not grid[x][y]:
-                return 0
-            
+            area = 0
+            stack = [[x,y]]
             grid[x][y] = 0
-            return 1 + explore(x-1, y) + explore(x+1, y) + explore(x, y-1) + explore(x, y+1)
+            while stack:
+                size = len(stack)
+                area += size
+                for _ in range(size):
+                    _x,_y = stack.pop()
+                    if _x > 0 and grid[_x-1][_y]:
+                        stack.append([_x-1, _y])
+                        grid[_x-1][_y] = 0
+                    
+                    if _x+1 < len(grid) and grid[_x+1][_y]:
+                        stack.append([_x+1, _y])
+                        grid[_x+1][_y] = 0
+                    
+                    if _y > 0 and grid[_x][_y-1]:
+                        stack.append([_x, _y-1])
+                        grid[_x][_y-1] = 0
+                        
+                    if _y+1 < len(grid[0]) and grid[_x][_y+1]:
+                        stack.append([_x, _y+1])
+                        grid[_x][_y+1] = 0
+            return area
+                    
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
