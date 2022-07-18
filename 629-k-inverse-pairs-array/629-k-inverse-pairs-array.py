a582a8575f13a83a891615541ada6c743b05fd26
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-07-18 12:27:56 +0600

    Time: 671 ms (58.75%), Space: 53.1 MB (40.00%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.py
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.py
@@ -3,14 +3,12 @@ from functools import lru_cache
 class Solution:
     def kInversePairs(self, n: int, k: int) -> int:
         dp = [[0]*(k+1) for _ in range(n+1)]
-        
+        M = 1000000007
         for i in range(1,n+1):
             dp[i][0] = 1
             for j in range(1, k+1):
-                # if i == 3:
-                    # print(i, j, j-i+1, j+1, dp[i-1][max(j-i+1, 0):j+1])
-                dp[i][j] = sum(dp[i-1][ max(j-i+1, 0) : j+1]) % 1000000007
-            # print(dp)
+                value = (dp[i-1][j] + M - (dp[i-1][j-i] if (j-i)>=0 else 0)) % M
+                dp[i][j] = (value + dp[i][j-1]) % M
         
-        return dp[-1][-1]
+        return (dp[n][k] + M - (dp[n][k-1] if k > 0 else 0)) % M
                 
