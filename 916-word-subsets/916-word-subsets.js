8dac56df759463ef8530849e542bb9d1a267fc2e
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-07-30 20:48:21 +0600

    Time: 420 ms (28.85%), Space: 77.3 MB (19.23%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string[]} words1
+ * @param {string[]} words2
+ * @return {string[]}
+ */
+var wordSubsets = function(words1, words2) {
+    
+    const frequency = {}
+    
+    for(let i = 0; i < words2.length; i++){
+        const tempFreq = {}
+        for (let j = 0; j < words2[i].length; j++){
+            const char = words2[i][j]
+            tempFreq[char] = (tempFreq[char] || 0) + 1
+        }
+        
+        Object.keys(tempFreq).forEach(key=> {
+            if(!frequency[key]){
+                frequency[key] = tempFreq[key]
+            }else{
+                frequency[key] = Math.max(frequency[key], tempFreq[key])
+            }
+        })
+    }
+    
+    const totalChars = Object.values(frequency).reduce((a,b)=> a + b, 0)
+    
+    console.log(frequency, totalChars)
+    
+    const matched = (word) => {
+        let tempTotal = totalChars
+        const tempFreq = {...frequency}
+        for(let char of word){
+            if(tempFreq[char]){
+                tempTotal -=1
+                tempFreq[char] -=1
+            }
+            
+            if(!tempTotal) return true
+        }
+        
+        return false
+    }
+    
+    return words1.filter(word => matched(word))
+    
+};
