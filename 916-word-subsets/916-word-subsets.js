59de8d6bc298f6b74370b5e2c9da129ec0c08248
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-07-30 21:28:57 +0600

    Time: 295 ms (59.61%), Space: 65.3 MB (40.38%) - LeetHub

--- 916-word-subsets/916-word-subsets.js
+++ 916-word-subsets/916-word-subsets.js
@@ -3,48 +3,23 @@
  * @param {string[]} words2
  * @return {string[]}
  */
-var wordSubsets = function(words1, words2) {
-    
-    // f = 26
-    const frequency = {}
-    
-    // m = word2 length
-    // i = average length of word in word 2
-    // O(m*i)
-    for(let i = 0; i < words2.length; i++){
-        const tempFreq = {}
-        for (let j = 0; j < words2[i].length; j++){
-            const char = words2[i][j]
-            tempFreq[char] = (tempFreq[char] || 0) + 1
+var wordSubsets = function(A, B) {
+    const count = (word) => {
+        const fq = new Array(26).fill(0)
+        for(let ch of word){
+            fq[ch.charCodeAt(0) - 'a'.charCodeAt(0)]++
         }
-        
-        Object.keys(tempFreq).forEach(key=> {
-            if(!frequency[key]){
-                frequency[key] = tempFreq[key]
-            }else{
-                frequency[key] = Math.max(frequency[key], tempFreq[key])
-            }
-        })
+        return fq
     }
     
-    const totalChars = Object.values(frequency).reduce((a,b)=> a + b, 0)
-
+    const BFreq = new Array(26).fill(0)
     
-    const matched = (word) => {
-        let tempTotal = totalChars
-        const tempFreq = {...frequency}
-        for(let char of word){
-            if(tempFreq[char]){
-                tempTotal -=1
-                tempFreq[char] -=1
-            }
-            
-            if(!tempTotal) return true
-        }
-        
-        return false
-    }
+    B.forEach(word=> {
+        count(word).forEach((v, i)=> BFreq[i] = Math.max(BFreq[i], v))
+    })
     
-    return words1.filter(word => matched(word))
+    const matched = (word) => !count(word).some((v, i) => BFreq[i] > v)
+    
+    return A.filter(word => matched(word))
     
 };
