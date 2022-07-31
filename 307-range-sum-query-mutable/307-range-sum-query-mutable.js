4061afc0153a3dc3cc5720adfd29912ce91cf775
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-07-31 21:14:51 +0600

    Time: 1074 ms (20.86%), Space: 101.3 MB (20.86%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.js
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.js
@@ -1,28 +1,64 @@
-/**
- * @param {number[]} nums
- */
-
+class TreeNode{
+    constructor(sum, start, end, left = null, right = null){
+        this.sum = sum
+        this.start = start
+        this.end = end
+        this.left = left
+        this.right = right
+    }
+}
 class NumArray{
     constructor(nums){
-        this.cs = [0]
-        for(let i = 0; i < nums.length; i++){
-            this.cs.push(nums[i] + this.cs[i])
+        this.tree = this.buildSegmentTree(0, nums.length-1, nums)
+    }
+    
+    buildSegmentTree(start,end, nums){
+        if(start === end) return new TreeNode(nums[start], start, end)
+        const mid = parseInt((start + end) / 2)
+        const left = this.buildSegmentTree(start, mid, nums)
+        const right = this.buildSegmentTree(mid+1, end, nums)
+        return new TreeNode(left.sum + right.sum, start, end, left, right)
+    }
+    
+    print(node, result = []){
+        if(node.start === node.end){
+            result.push(node.sum)
+            return
         }
         
-        // console.log('create', this.cs)
+        this.print(node.left, result)
+        this.print(node.right, result)
+        return result
     }
     
-    update(index, val){
-        const diff = val - this.sumRange(index, index)
-        for(let i = index+1; i < this.cs.length; i++){
-            this.cs[i] += diff
+    update(index, val, nodeHead = null){
+        const node = nodeHead || this.tree
+        const mid = parseInt((node.start + node.end)/2)
+        
+        if(node.start === node.end){
+            node.sum = val
+            return
+        }else if(index <= mid){
+            this.update(index, val, node.left)
+        }else{
+            this.update(index, val, node.right)
         }
-        // console.log('update', this.cs)
+        
+        node.sum = node.left.sum + node.right.sum
     }
     
-    sumRange(left, right){
-        return this.cs[right+1]-this.cs[left]
-        // console.log('sumRange', this.cs)
+    sumRange(start, end, nodeHead = null){
+        const node = nodeHead || this.tree
+        if(node.start === start && node.end === end) return node.sum
+        const mid = parseInt((node.start + node.end)/2)
+        
+        if(start <= mid && end<= mid){
+            return this.sumRange(start, end, node.left)
+        }else if(start > mid && end > mid){
+            return this.sumRange(start, end, node.right)
+        }else{
+            return this.sumRange(start, mid, node.left) + this.sumRange(mid+1, end, node.right)
+        }
     }
 }
 
