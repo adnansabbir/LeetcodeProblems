867687bbcb14fdba5458e7bf5a8b024a97e01444
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-08-01 10:11:32 +0600

    Time: 48 ms (54.87%), Space: 13.8 MB (73.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -2,6 +2,8 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         if m == 1 or n == 1:
             return 1
+        elif m == 2 or n == 2:
+            return max([m, n])
         
         dp = [0]*n
         dp[0] = 1
