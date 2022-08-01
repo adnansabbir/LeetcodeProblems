7ec67417692214248cfeb26189e7d75a6ee07646
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-08-01 09:35:33 +0600

    Time: 48 ms (54.87%), Space: 14.1 MB (11.02%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,12 +1,22 @@
-from functools import lru_cache
 class Solution:
-    @lru_cache(maxsize=None)
-    def uniquePaths(self, m: int, n: int) -> int:
+    def uniquePaths(self, m: int, n: int, cache = {}) -> int:
+        
         if m<1 or n<1:
             return 0
         
-        if m == 1 and n == 1:
+        if m == 1 and n==1:
             return 1
         
-        return self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+        key = "{}.{}".format(m,n)
+        
+        if key in cache:
+            return cache[key]
+        
+        top = self.uniquePaths(m-1, n, cache)
+        left = self.uniquePaths(m, n-1, cache)
+        
+        cache[key] = left + top
+        
+        return cache[key]
+        
         
