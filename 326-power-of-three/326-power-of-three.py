0a9f78007eabdadffc72c9e588f4f5048f5caa49
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-08-24 08:56:56 +0600

    Time: 131 ms (53.84%), Space: 13.9 MB (57.97%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -1,6 +1,5 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        maxNumber = math.pow(3,19)
-        return n>0 and maxNumber%n==0
+        return n>0 and 1162261467%n==0
             
         
