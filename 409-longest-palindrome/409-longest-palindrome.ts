9b5b11c7877b194278afc6993af072962faa08d5
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-08-26 23:06:06 +0600

    Time: 106 ms (61.34%), Space: 44.7 MB (85.29%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.ts
+++ 409-longest-palindrome/409-longest-palindrome.ts
@@ -1,14 +1,17 @@
 function longestPalindrome(s: string): number {
     const frequency = {}
     let size = 0
+    const oddChars = new Set<string>()
     for(let char of s){
         frequency[char] = (frequency[char] || 0) + 1
+        oddChars.add(char)
         if(frequency[char] === 2){
             size += frequency[char]
             frequency[char] = 0
+            oddChars.delete(char)
         }
     }
     
-    let extraSize = Object.keys(frequency).some(key=> !!frequency[key]) ? 1 : 0
+    let extraSize = oddChars.size ? 1 : 0
     return size + extraSize
 };
