65ea97c7fd116f5cd624f41672c5c99ce78259ff
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-08-26 10:27:37 +0600

    Time: 48 ms (73.80%), Space: 13.8 MB (64.17%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -4,14 +4,12 @@ class Solution:
         digitCount = len(str(n))
         sortedN = ''.join(sorted(str(n)))
         window = [10**(digitCount-1), 10**(digitCount) - 1]
-        possibleResults = set()
         for i in range(1000):
             if 2**i > window[1]:
                 break
             if 2**i >= window[0]:
-                possibleResults.add(''.join(sorted(str(2**i))))
-        
-            if sortedN in possibleResults: return True
+                if ''.join(sorted(str(2**i))) == sortedN:
+                    return True
         return False
             
         
