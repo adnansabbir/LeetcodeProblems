85a475b39180f1ad4986310882e6856867ebb647
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-09-09 23:37:48 +0600

    Updated

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -5,7 +5,6 @@
  */
 var twoSum = function(nums, target) {
     const idx_map = {}
-    
     for (let i=0; i<nums.length; i++){
         if(idx_map[target-nums[i]] !== undefined){
             return [idx_map[target-nums[i]], i]
