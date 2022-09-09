610dfa8d7420888c8c41fdc9d0b06e2ef33d34bc
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-09-10 01:20:59 +0600

    Time: 894 ms (100.00%), Space: 98.7 MB (100.00%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.ts
@@ -0,0 +1,56 @@
+function numberOfWeakCharacters(properties: number[][]): number {    
+    properties = properties.sort((a,b)=> a[0]===b[0] ? a[1]-b[1] : a[0]-b[0])
+    const size = properties.length - 1
+    const getIndexOfNextLargerAttack=(value: number):number=>{
+        let start = 0
+        let end = size
+        
+        while(start<=end){
+            const mid = Math.floor((start+end)/2)
+            if(properties[mid][0]>value && properties[mid-1][0]==value) return mid
+            else if(properties[mid][0]<=value){
+                start=mid+1
+            }else{
+                end=mid-1
+            }
+        }
+        return -1
+    }
+    
+    const largestFromLast = new Array<number>(properties.length)
+    for(let i = size; i>=0; i--){
+        if(i==size){
+            largestFromLast[i] = properties[i][1]
+        }else{
+            largestFromLast[i] = Math.max(largestFromLast[i+1], properties[i][1])
+        }
+    }
+    
+    const hasLargerDefence=(start:number, value: number):boolean=>{
+        let end = size
+        while(start<=end){
+            const mid = Math.floor((start+end)/2)
+            if(largestFromLast[mid]>value) return true
+            else if(largestFromLast[mid]<=value){
+                end=mid-1
+            }else{
+                start=mid+1
+            }
+        }
+        return false
+    }
+
+    let weakCaharacters = 0
+    // console.log(properties)
+    // console.log(largestFromLast)
+    for (let prop of properties){
+        const largerAttackIndex = getIndexOfNextLargerAttack(prop[0])
+        if(largerAttackIndex===-1) continue
+        if(hasLargerDefence(largerAttackIndex, prop[1])){
+            weakCaharacters++
+        }
+        // console.log(prop, largerAttackIndex, hasLargerDefence(largerAttackIndex, prop[1]))
+    }
+    
+    return weakCaharacters
+};
