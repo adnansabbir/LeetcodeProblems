7274e96e53dfc16d60b2cf7e2cedf4be55ad8bb9
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-09-10 23:35:41 +0600

    Time: 603 ms (6.25%), Space: 84.9 MB (6.25%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.ts
@@ -0,0 +1,24 @@
+function maxProfit(k: number, prices: number[]): number {
+    if(k==0 || prices.length <= 1) return 0
+    const dp = {}
+    let count = 0
+    const getMaxProfit = (cidx: number, buying: boolean, transaction: number) => {
+        if(transaction === 0 || cidx === prices.length) return 0
+        count+=1
+        const key = `(${cidx})(${buying})(${transaction})`
+        if(dp[key]!== undefined) return dp[key]
+        
+        // skip
+        let profit = getMaxProfit(cidx + 1, buying, transaction)
+        if(buying){
+            profit = Math.max(profit, getMaxProfit(cidx + 1, !buying, transaction) - prices[cidx])
+        }else{
+            profit = Math.max(profit, getMaxProfit(cidx + 1, !buying, transaction - 1) + prices[cidx])
+        }
+        
+        dp[key] = profit
+        return dp[key]
+    }
+    const result = getMaxProfit(0, true, k)
+    return result
+};
