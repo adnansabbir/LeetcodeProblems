e1d065bca0d8abdfdd2cf8420ddf2d3d6f5ddef4
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-09-20 12:39:49 +0600

    Time: 366 ms (76.19%), Space: 45.3 MB (76.19%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.ts
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.ts
@@ -1,12 +1,14 @@
 function findLength(nums1: number[], nums2: number[]): number {
-    // const dp = new Array<number[]>(2).fill([]).map(_ => new Array<number>(nums2.length).fill(0))
-    const dp = new Array<number[]>(nums1.length).fill(null).map(_ => new Array<number>(nums2.length).fill(0))
+    const dp = new Array<number[]>(2).fill([]).map(_ => new Array<number>(nums2.length).fill(0))
+    // const dp = new Array<number[]>(nums1.length).fill(null).map(_ => new Array<number>(nums2.length).fill(0))
     let result = 0
     for(let i = 0; i<nums1.length; i++){
         for(let j = 0; j<nums2.length; j++){
             if(nums1[i] === nums2[j]){
-                dp[i][j] = 1 + ((j > 0 && i > 0) ? dp[i-1][j-1] : 0)
-                result = Math.max(result, dp[i][j])
+                dp[i%2][j] = 1 + (j > 0 ? dp[(i+1)%2][j-1] : 0)
+                result = Math.max(result, dp[i%2][j])
+            }else{
+                dp[i%2][j] = 0
             }
         }
     }
