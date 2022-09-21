a3e4f76fddb2801d340171765cd20759f067d67b
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-09-21 22:28:33 +0600

    Time: 95 ms (94.50%), Space: 49.1 MB (87.16%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.ts
+++ 41-first-missing-positive/41-first-missing-positive.ts
@@ -1,8 +1,13 @@
 function firstMissingPositive(nums: number[]): number {
+    // using array itself as hashmap
+    
     let start = 0
+    // we try to put each num in it's num-1 position
+    // if the position is outside of array or
+    
     while(start < nums.length){
         const index = nums[start] - 1
-        if(index < 0 || index>=nums.length || start + 1 === nums[start] || nums[index] === index+1){
+        if(index < 0 || index>=nums.length || nums[index] === index+1){
             start++
             continue
         }
