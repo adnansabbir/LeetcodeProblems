04929d76c7ce64401d08635ac2058121b5f368b9
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-09-21 22:26:27 +0600

    Time: 151 ms (54.59%), Space: 48.6 MB (91.28%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.ts
+++ 41-first-missing-positive/41-first-missing-positive.ts
@@ -1,8 +1,17 @@
 function firstMissingPositive(nums: number[]): number {
-    const uniqueNums = new Set(nums)
-    for(let i = 1; i<= nums.length; i++){
-        if(!uniqueNums.has(i)) return i
+    let start = 0
+    while(start < nums.length){
+        const index = nums[start] - 1
+        if(index < 0 || index>=nums.length || start + 1 === nums[start] || nums[index] === index+1){
+            start++
+            continue
+        }
+        
+        nums[start] = nums[index]
+        nums[index] = index + 1
     }
-    
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i] !== i+1) return i+1
+    }
     return nums.length + 1
 };
