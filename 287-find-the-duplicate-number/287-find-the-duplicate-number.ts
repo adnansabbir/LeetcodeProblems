ad9e7ce6a54c85036f0e9f6271f2377389dd30e2
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-09-21 21:59:36 +0600

    Time: 141 ms (62.50%), Space: 51.7 MB (64.88%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.ts
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.ts
@@ -1,11 +1,17 @@
 function findDuplicate(nums: number[]): number {
-    // Solution by negetive marketing strategy
-    for(let num of nums){
-        const pointer = Math.abs(num)
-        if(nums[pointer] < 0){
-            return pointer
+    // Array as hashmap approach
+    // As we have one extra number and no 0 will be there, we will try to map the numbers to it's correct index
+    // if we find the number is already there then it's a duplicate
+    let index = 0
+    while(index < nums.length){
+        const nextIndex = nums[index]
+        if(index === nums[nextIndex]){
+            index++
+            continue
         }
-        
-        nums[pointer] *= -1
+        if(nums[index] === nums[nextIndex]) return nextIndex
+        nums[index] = nums[nextIndex]
+        nums[nextIndex] = nextIndex
     }
+    
 };
