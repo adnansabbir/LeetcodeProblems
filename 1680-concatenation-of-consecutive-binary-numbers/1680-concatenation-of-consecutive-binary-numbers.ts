311f0aed840869d8dbb68c04c00ffc8ca2f4ac02
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-09-23 16:53:55 +0600

    Time: 5802 ms (50.00%), Space: 48.8 MB (50.00%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.ts
@@ -0,0 +1,12 @@
+function concatenatedBinary(n: number): number {
+    let binaryResult = ""
+    const MOD = BigInt(10**9 + 7)
+    
+    for(let i = 1; i <= n; i++){
+        binaryResult += i.toString(2)
+        const temp = BigInt('0b' + binaryResult)
+        binaryResult = (temp%MOD).toString(2)
+    }
+
+    return parseInt(binaryResult, 2)
+};
