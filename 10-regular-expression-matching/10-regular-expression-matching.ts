ec308d6c49a9f4aed6a305c8cac0f78b4e43ffeb
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-10-03 15:12:18 +0600

    Time: 124 ms (82.93%), Space: 45.9 MB (49.39%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.ts
+++ 10-regular-expression-matching/10-regular-expression-matching.ts
@@ -1,9 +1,9 @@
 function isMatch(s: string, p: string): boolean {
-    // s = 'acdd'
-    // p = 'a.*d'
-    
-    
+    const cache: Record<string, boolean> = {}
     const dp = (i:number, j:number) => {
+        const key = `${i}-${j}`
+        if(cache[key] !== undefined) return cache[key]
+        
         let ans = false
         if(j === p.length){
             ans = i === s.length
@@ -17,8 +17,9 @@ function isMatch(s: string, p: string): boolean {
             }
         }
         
+        cache[key] = ans
         return ans
     }
-    
+
     return dp(0,0)    
 };
