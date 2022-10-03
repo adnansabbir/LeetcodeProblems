b1e15b6bf671c5d69a05b079574175247bc46a9d
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-10-03 08:30:42 +0600

    Time: 159 ms (80.00%), Space: 53.6 MB (50.00%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.ts
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.ts
@@ -1,8 +1,4 @@
 function minCost(colors: string, neededTime: number[]): number {
-    // "a   a   a   a   b   b   b   a   b   b   b   b"
-    // [5,  3,  3,  5,  7,  5,  3,  5,  5,  4,  8,  1]
-    //                                  l   r
-    // mintime = 19
     let left = 0, right = 1
     let minCost = 0
     
