6fdf901cc3eeee0275776d4fd53cdc5db7efa849
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-10-03 15:04:33 +0600

    Time: 358 ms (9.75%), Space: 49.1 MB (20.73%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.ts
+++ 10-regular-expression-matching/10-regular-expression-matching.ts
@@ -2,15 +2,23 @@ function isMatch(s: string, p: string): boolean {
     // s = 'acdd'
     // p = 'a.*d'
     
-    if(!p) return !s
     
-    const firstElemMatched = s.length && [s[0], '.'].includes(p[0])
-    
-    if(p.length > 1 && p[1] === '*'){
-        return isMatch(s, p.substring(2)) || firstElemMatched && isMatch(s.substring(1), p)
-    }else{
-        return firstElemMatched && isMatch(s.substring(1), p.substring(1))
+    const dp = (i:number, j:number) => {
+        let ans = false
+        if(j === p.length){
+            ans = i === s.length
+        }else{
+            const firstElemMatched = i < s.length && [s[i], '.'].includes(p[j])
+            
+            if(j+1 < p.length && p[j+1] === '*'){
+                ans = dp(i, j+2) || (firstElemMatched && dp(i+1, j)) 
+            }else{
+                ans = firstElemMatched && dp(i+1, j+1)
+            }
+        }
+        
+        return ans
     }
     
-    
+    return dp(0,0)    
 };
