6aefa6bcf01ffabb8d22a4cd75873b7ef31c4784
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-10-20 01:35:25 +0600

    Time: 106 ms (51.41%), Space: 44.1 MB (48.59%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.ts
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.ts
@@ -1,12 +1,16 @@
 function findMin(nums: number[]): number {
-    const getSmallest = (start: number, end: number) => {
-        if(start === end) return nums[start]
-        // if there is one element retur that as min
-        
-        if(nums[start] < nums[end]) return nums[start]
-        const mid = Math.floor((start+end)/2)
-        return Math.min(getSmallest(start, mid), getSmallest(mid+1, end))
-    }
+    let start = 0, end = nums.length - 1
     
-    return getSmallest(0, nums.length-1)
+    if(nums[start] <= nums[end]) return nums[start]
+    
+    while(start < end){
+        const mid = Math.floor((start+end)/2)
+        if(nums[mid] > nums[mid+1]) return nums[mid+1]
+        else if(nums[mid] < nums[mid-1]) return nums[mid]
+        else if(nums[mid] > nums[0]){
+            start = mid+1
+        }else{
+            end = mid-1
+        }
+    }
 };
