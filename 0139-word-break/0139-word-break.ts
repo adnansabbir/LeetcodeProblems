ef651213980b81a06a9c32da727b48913098a2e0
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-10-20 15:02:03 +0600

    Time: 112 ms (60.17%), Space: 44.4 MB (83.12%) - LeetHub

--- 0139-word-break/0139-word-break.ts
+++ 0139-word-break/0139-word-break.ts
@@ -1,26 +1,15 @@
 function wordBreak(s: string, wordDict: string[]): boolean {
-    // app le [a, app, l, e]
+    const dp = new Array<boolean>(s.length).fill(false)
+    dp.push(true)
     
-    const wordSets = new Set<string>(wordDict)
-    
-    const dp:Record<string, boolean> = {}
-    const canFormWord = (str: string): boolean =>{
-        if(dp[str] !== undefined) return dp[str]
-        if(wordSets.has(str)) return true
-        if(str.length === 1) return false
-        
-        for(let i = 1; i<str.length; i++){
-            const left = str.substring(0,i)
-            const right = str.substring(i)
-            if(canFormWord(left) && canFormWord(right)) {
-                dp[str] = true
-                return true
+    for(let i = s.length - 1; i>=0; i--){
+        for(let word of wordDict){
+            if(s.substring(i).startsWith(word)){
+                dp[i] = dp[i+word.length]
             }
+            if(dp[i]) break
         }
-        
-        dp[str] = false
-        return false
     }
-    
-    return canFormWord(s)
+    console.log(dp)
+    return dp[0]
 };
