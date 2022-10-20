9ec617e2aceb70c6efef68ac5fe615ba6544380e
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-10-20 12:41:12 +0600

    Time: 71 ms (89.11%), Space: 42.6 MB (72.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.ts
+++ 0070-climbing-stairs/0070-climbing-stairs.ts
@@ -1,6 +1,12 @@
-function climbStairs(n: number, memo = {}): number {
-    if(n <= 3 ) return n
-    if(memo[n] !== undefined) return memo[n]
-    memo[n] = climbStairs(n-1, memo) + climbStairs(n-2,memo)
-    return memo[n]
+function climbStairs(n: number): number {
+    const dp = new Array<number>(n+1)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    
+    for(let i = 3; i <= n; i++){
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+    
+    return dp[n]
 };
