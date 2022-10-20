319b14368e85a55f0ae5d966f8020965b4f726e4
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-10-20 14:00:14 +0600

    Time: 110 ms (95.12%), Space: 57.7 MB (37.40%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.ts
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.ts
@@ -2,14 +2,6 @@ function longestCommonSubsequence(text1: string, text2: string): number {
     const dp = new Array<number[]>(text1.length).fill([]).map(_ => new Array<number>(text2.length).fill(0))
     
     const getValue = (i: number, j:number) => i < 0 || j < 0 ? 0 : dp[i][j]
-    
-    //     a   a   e
-    // a   1   1   1   
-    // b   1   1
-    // c
-    // d
-    // e
-    
     for(let t1 = 0; t1 < dp.length; t1++){
         for(let t2 = 0; t2 < dp[0].length; t2++){
             if(text1[t1] === text2[t2]){
