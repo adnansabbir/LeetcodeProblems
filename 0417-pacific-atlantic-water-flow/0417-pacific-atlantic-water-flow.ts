068fe372c04a4913e5ebd248fd2f103baa7f7c6f
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-10-22 03:18:24 +0600

    Time: 427 ms (22.13%), Space: 54.1 MB (26.88%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.ts
@@ -0,0 +1,59 @@
+function pacificAtlantic(heights: number[][]): number[][] {
+    // null -> unchecked
+    // -1 -> noFlow
+    // 0 -> checking
+    // 1 -> pacific ocean
+    // 2 -> atlantic ocean
+    // 3 -> both
+    
+    const H = heights.length
+    const W = heights[0].length
+    const flow = new Array<number[]>(H).fill([]).map(_ => new Array<number>(W))
+    
+    const getConnectedOcean = (h: number, w: number): number => {
+        if(h<0 || w<0) return 1
+        if(h>=H || w>=W) return 2
+        return 0
+    }
+    
+    const calculateFlow = (h: number, w:number): number => {
+        if(flow[h][w] !== undefined) return flow[h][w]
+        flow[h][w] = 0
+        const oceans = new Set<number>()
+        
+        const neighbors = [[h-1, w], [h+1, w], [h,w-1], [h,w+1]]
+        neighbors.forEach(([nh, nw]) => {
+            if(getConnectedOcean(nh, nw) === 0 && heights[h][w] >= heights[nh][nw]) oceans.add(calculateFlow(nh, nw))
+            else oceans.add(getConnectedOcean(nh, nw))
+        })
+        // console.log(h,w, oceans)
+        
+        if(oceans.has(3) || (oceans.has(2) && oceans.has(1))) flow[h][w] = 3
+        else if(oceans.has(2)) flow[h][w] = 2
+        else if(oceans.has(1)) flow[h][w] = 1
+        
+        if(oceans.has(0) && flow[h][w] !== 3){
+            const result = flow[h][w]
+            flow[h][w] = undefined
+            return result
+        }
+        
+        return flow[h][w]
+    }
+    
+//     [[5,5,5,5],
+//      [5,1,1,5],
+//      [5,1,5,5],
+//      [5,5,5,5]
+//     ]
+    
+    const result = []
+    for(let height = 0; height < H; height++){
+        for(let width = 0; width < W; width++){
+            calculateFlow(height, width)
+            if(flow[height][width] === 3) result.push([height, width])
+        }
+    }
+    // console.log(flow)
+    return result
+};
