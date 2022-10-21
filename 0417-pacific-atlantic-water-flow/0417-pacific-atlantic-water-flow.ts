ecb72ab376255a475ba08de40d7f720ecf87d017
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-10-22 04:28:53 +0600

    Time: 472 ms (20.55%), Space: 55.8 MB (22.53%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.ts
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.ts
@@ -1,57 +1,67 @@
 function pacificAtlantic(heights: number[][]): number[][] {
-    // null -> unchecked
-    // -1 -> noFlow
-    // 0 -> checking
+    // undefined -> unchecked
+    // 0 -> no flow
     // 1 -> pacific ocean
     // 2 -> atlantic ocean
     // 3 -> both
     
-    const H = heights.length
-    const W = heights[0].length
-    const flow = new Array<number[]>(H).fill([]).map(_ => new Array<number>(W))
+    const R = heights.length
+    const C = heights[0].length
+    const flow = new Array<number[]>(R).fill([]).map(_ => new Array<number>(C))
     
-    const getConnectedOcean = (h: number, w: number): number => {
-        if(h<0 || w<0) return 1
-        if(h>=H || w>=W) return 2
-        return 0
+    const setValue = (r: number, c: number, ocean: number) => {
+        if(flow[r][c] === 3) return
+        else if(flow[r][c] === undefined) flow[r][c] = ocean
+        else if(flow[r][c] + ocean === 3) flow[r][c] = 3
     }
     
-    const calculateFlow = (h: number, w:number): number => {
-        if(flow[h][w] !== undefined) return flow[h][w]
-        flow[h][w] = 0
-        const oceans = new Set<number>()
-        
-        const neighbors = [[h-1, w], [h+1, w], [h,w-1], [h,w+1]].filter(([nh, nw]) => getConnectedOcean(nh,nw) || heights[h][w] >= heights[nh][nw])
-        neighbors.forEach(([nh, nw]) => {
-            if(getConnectedOcean(nh, nw) === 0 && heights[h][w] >= heights[nh][nw]) oceans.add(calculateFlow(nh, nw))
-            else oceans.add(getConnectedOcean(nh, nw))
-        })
-        // console.log(h,w, oceans)
-        
-        if(oceans.has(3) || (oceans.has(2) && oceans.has(1))) flow[h][w] = 3
-        else if(oceans.has(2)) flow[h][w] = 2
-        else if(oceans.has(1)) flow[h][w] = 1
-        
-        if(oceans.has(0) && flow[h][w] !== 3){
-            const result = flow[h][w]
-            flow[h][w] = undefined
-            return result
+    const getNeighborsGreaterOrEqual = (r: number, c: number) => {
+        const neighbors = [[r-1,c], [r+1,c],[r,c-1],[r,c+1]].filter(([nr,nc]) => !(nr<0 || nc<0 || nr>=R || nc>=C))
+        return neighbors.filter(([nr,nc]) => heights[r][c] <= heights[nr][nc])
+    }
+
+    let queue: number[][] = []
+    const connectOceans = (q: number[][], ocean: number) => {
+        const visited = new Set<string>()
+        while(q.length){
+            const size = q.length
+            for(let i = 0; i<size; i++){
+                const [nr, nc] = q.shift()
+                setValue(nr, nc, ocean)
+                visited.add(`${nr}-${nc}`)
+                const neighbors = getNeighborsGreaterOrEqual(nr,nc).filter(([r,c]) => !visited.has(`${r}-${c}`))
+                q.push(...neighbors)
+            }
         }
-        
-        return flow[h][w]
     }
-    
-//     [[5,5,5,5],
-//      [5,1,1,5],
-//      [5,1,5,5],
-//      [5,5,5,5]
-//     ]
+
+    // putting pacific and atlantic to left and right column
+    for(let r = 0; r<R; r++){
+        for(let c = 0; c<C; c++){
+            if(r===0 || c===0){
+                queue.push([r,c])
+            }
+        }
+    }
+    connectOceans(queue, 1)
+
+    queue = []
+    // putting pacific and atlantic to top and bottom
+    for(let r = 0; r<R; r++){
+        for(let c = 0; c<C; c++){
+            if(r+1===R || c+1===C){
+                queue.push([r,c])
+            }
+        }
+    }
+    connectOceans(queue, 2)
     
     const result = []
-    for(let height = 0; height < H; height++){
-        for(let width = 0; width < W; width++){
-            calculateFlow(height, width)
-            if(flow[height][width] === 3) result.push([height, width])
+    for(let r = 0; r<R; r++){
+        for(let c = 0; c<C; c++){
+            if(flow[r][c] === 3){
+                result.push([r,c])
+            }
         }
     }
     // console.log(flow)
