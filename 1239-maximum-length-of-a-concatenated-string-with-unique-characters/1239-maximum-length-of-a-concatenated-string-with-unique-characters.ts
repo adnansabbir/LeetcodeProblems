31bed66e2f7fa1fa929c950b91b90782b7e0a0a2
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-10-24 11:31:16 +0600

    Time: 1467 ms (33.33%), Space: 50.4 MB (33.33%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.ts
@@ -0,0 +1,24 @@
+function maxLength(arr: string[]): number {
+    let maxLength = 0
+    
+    const hasUniqueCharacters = (str: string): boolean => {
+        const avoid = new Set<string>()
+        for(let i = 0; i<str.length; i++){
+            if(avoid.has(str[i])) return false
+            else avoid.add(str[i])
+        }
+        return true
+    }
+    
+    const traverse = (currString: string, idx: number): number => {
+        if(idx >= arr.length) return hasUniqueCharacters(currString) ? currString.length : 0
+        
+        const lengthWithCurr = traverse(currString + arr[idx], idx+1)
+        if(lengthWithCurr === 26) return 26
+        const lengthWithoutCurr = traverse(currString, idx+1)
+        
+        return Math.max(lengthWithCurr, lengthWithoutCurr)
+    }
+    
+    return traverse('', 0)
+};
