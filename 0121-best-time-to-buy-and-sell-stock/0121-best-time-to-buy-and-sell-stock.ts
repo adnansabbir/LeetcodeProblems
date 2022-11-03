4b6eaba8dc9dbdcfae87384f0a8d62ea0f821d96
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2022-11-04 02:47:14 +0600

    Time: 145 ms (66.47%), Space: 52.3 MB (24.24%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,11 +1,11 @@
 function maxProfit(prices: number[]): number {
-    let buyingPrice = prices[0]
     let profit = 0
-    for(let i = 1; i < prices.length; i++){
-        if(prices[i] > buyingPrice){
-            profit = Math.max(profit, prices[i] - buyingPrice)
+    let lowestPrice = prices[0]
+    for(let price of prices){
+        if(price < lowestPrice){
+            lowestPrice = price
         }else{
-            buyingPrice = prices[i]
+            profit = Math.max(profit, price - lowestPrice)
         }
     }
     
