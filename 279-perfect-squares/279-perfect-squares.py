4b261c4d37a6b72949935668372fc2e17824884a
adnansabbir <adnan.sabbir2012@gmail.com>
2022-11-22 22:44:17 +0600

    removed comments

--- 279-perfect-squares/279-perfect-squares.py
+++ 279-perfect-squares/279-perfect-squares.py
@@ -1,15 +1,4 @@
 class Solution:
-#     def numSquares(self, n: int) -> int:        
-#         dp = [n] * (n+1)
-#         dp[0] = 0
-        
-#         for i in range(1, n+1):
-#             for j in range(1, int(sqrt(n)) + 1):
-#                 val = j * j
-#                 if i - val >= 0:
-#                     dp[i] = min(dp[i], 1+dp[i-val])
-#         return dp[-1]
-    
     def numSquares(self, n: int) -> int:        
         dp = [n] * (n+1)
         dp[0] = 0
