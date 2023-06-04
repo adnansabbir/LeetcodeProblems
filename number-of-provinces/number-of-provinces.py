2c6b76c19368deef0f178a0b96895cd1f9e73fe6
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2023-06-04 12:32:03 +0600

    Time: 200 ms (45.44%) | Memory: 17 MB (22.10%) - LeetSync

--- number-of-provinces/number-of-provinces.py
+++ number-of-provinces/number-of-provinces.py
@@ -2,14 +2,7 @@ class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         provinces = 0
         n = len(isConnected)
-
-        #     0   1   2   3
-
-        # 0   1   1   0   1
-        # 1   1   1   0   0
-        # 2   0   0   1   0
-        # 3   1   0   0   1
-
+        
         def markProvince(curr: int)-> int:
             if isConnected[curr][curr] != 1:
                 return 0
