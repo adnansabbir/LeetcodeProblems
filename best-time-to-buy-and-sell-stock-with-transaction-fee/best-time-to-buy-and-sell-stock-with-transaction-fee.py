f5e3f208173fd242b4c5fe7183fe32df198b7ca2
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2023-06-23 00:10:47 +0600

    Time: 689 ms (81.46%) | Memory: 23.7 MB (49.97%) - LeetSync

--- best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -3,7 +3,6 @@ class Solution:
         n = len(prices)
         hold, free =0, 0
         
-        # In order to hold a stock on day 0, we have no other choice but to buy it for prices[0].
         hold = -prices[0]
         
         for i in range(1, n):
