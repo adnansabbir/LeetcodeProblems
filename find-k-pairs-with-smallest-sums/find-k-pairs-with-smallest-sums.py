a21edc440205bd3fc36d6f9ec20635a4c866e451
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2023-06-28 03:06:54 +0600

    Time: 1198 ms (26.07%) | Memory: 43 MB (8.00%) - LeetSync

--- find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
+++ find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
@@ -1,27 +1,44 @@
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        # put into heap
+        # pop smallest (index tuple)
+        # put neighbouring index tuple into heap
         from heapq import heappush, heappop
-        m = len(nums1)
-        n = len(nums2)
 
         ans = []
-        visited = set()
+        visited = set((0,0))
+        indexDict = {}
+        n,m = len(nums1), len(nums2)
 
-        minHeap = [(nums1[0] + nums2[0], (0, 0))]
-        visited.add((0, 0))
-        count = 0
+        for i in range(n):
+            indexDict[i] = 0
 
-        while k > 0 and minHeap:
-            val, (i, j) = heappop(minHeap)
+        def getNeighbouringSmallNumberIndexes(i: int)-> List[int]:
+            result = []
+            if i-1 in indexDict and indexDict[i-1] < m and (i-1, indexDict[i-1]) not in visited:
+                result.append((i-1, indexDict[i-1]))
+            
+            if indexDict[i] < m and (i, indexDict[i]) not in visited:
+                result.append((i, indexDict[i]))
+
+            if i+1 in indexDict and indexDict[i+1] < m and (i+1, indexDict[i+1]) not in visited:
+                result.append((i+1, indexDict[i+1]))
+
+            return result
+
+        heap = [(nums1[0] + nums2[0], (0, 0))]
+        while k and heap:
+            k-=1
+            _, [i, j] = heappop(heap)
+            indexDict[i] = j + 1
             ans.append([nums1[i], nums2[j]])
 
-            if i + 1 < m and (i + 1, j) not in visited:
-                heappush(minHeap, (nums1[i + 1] + nums2[j], (i + 1, j)))
-                visited.add((i + 1, j))
+            for [ni, nj] in getNeighbouringSmallNumberIndexes(i):
+                heappush(heap, (nums1[ni] + nums2[nj], (ni, nj)))
+                visited.add((ni, nj))
+            # print(heap)
 
-            if j + 1 < n and (i, j + 1) not in visited:
-                heappush(minHeap, (nums1[i] + nums2[j + 1], (i, j + 1)))
-                visited.add((i, j + 1))
-            k = k - 1
         
         return ans
+
+        
