a166d6cf74c4f460b7b2a5a7c556bf487632a9f7
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2023-06-30 19:24:35 +0600

    Time: 4185 ms (8.69%) | Memory: 27.6 MB (47.83%) - LeetSync

---
+++ last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.py
@@ -0,0 +1,42 @@
+class Solution:
+    def latestDayToCross(self, row: int, col: int, cells: List[List[int]]) -> int:
+        # fill all blocks will possible paths which is 4
+        # Every time we fill one block with water reduce one from the blocks dependent on it
+        # Keep track of all row, till any of the row has atleast one possible path it might be possible to reach end
+         
+        filledOnDay = [[None]*col for r in range(row)]
+        for i, [r,c] in enumerate(cells):
+            filledOnDay[r-1][c-1] = i+1
+        
+        def getNeighbours(pos: Tuple[int], currDay: int, visited: set)-> List[Tuple[int]]:
+            [r, c] = pos
+            neighbour = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+            return [(x, y) for [x, y] in neighbour if 0 <= x < row and 0 <= y < col and (x, y) not in visited and filledOnDay[x][y] > currDay]
+
+        def canReachBottom(currDay: int)-> bool:
+            queue = [(0, c) for c in range(col) if filledOnDay[0][c] > currDay]
+            visited = set(queue)
+            while queue:
+                size = len(queue)
+                for _ in range(size):
+                    [r, c] = queue.pop(0)
+                    if r == row - 1:
+                        return True
+                    
+                    neighbours = getNeighbours((r,c), currDay, visited)
+                    for neigh in neighbours:
+                        visited.add(neigh)
+                        queue.append(neigh)
+                    # queue = queue + neighbours
+            return False
+        
+        start, end = 0, len(cells)
+        result = 0
+        while start <= end:
+            mid = (start + end) // 2
+            if canReachBottom(mid):
+                result = max(result, mid)
+                start = mid + 1
+            else:
+                end = mid - 1
+        return result
