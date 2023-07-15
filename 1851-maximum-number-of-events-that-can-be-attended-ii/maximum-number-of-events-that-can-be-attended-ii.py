a6faa040e3796b13203b87614792cdc4b9ba2c1e
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2023-07-15 20:54:08 +0600

    Time: 1217 ms (20.00%) | Memory: 158 MB (48.89%) - LeetSync

---
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.py
@@ -0,0 +1,44 @@
+from functools import lru_cache
+
+class Solution:
+    def maxValue(self, events: List[List[int]], k: int) -> int:
+        n = len(events)
+        sortedEvents = sorted(events, key = lambda x: (x[0], x[1], -x[2]))
+        # print(sortedEvents)
+
+        def getNextEventIndex(fromIdx: int, busyTill: int)-> int:
+            start, end = fromIdx + 1, n - 1
+            while start <= end:
+                mid = (start + end) // 2
+                if sortedEvents[mid][0] > busyTill and sortedEvents[mid - 1][0] <= busyTill:
+                    return mid
+                elif sortedEvents[mid][0] <= busyTill:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            return n
+        
+        # lru_cache(maxsize = None)
+        cache = [[None]*(k + 1) for _ in range(n)]
+        def dp(currIdx: int, remainingEvents: int)-> int:
+            if currIdx >= n or not remainingEvents:
+                return 0
+
+            if cache[currIdx][remainingEvents] != None:
+                return cache[currIdx][remainingEvents]
+            
+            [start, end, value] = sortedEvents[currIdx]
+            withCurrent = 0
+            withOutCurrent = 0
+            
+            withOutCurrent = dp(currIdx + 1, remainingEvents)
+            nextEventIdx = getNextEventIndex(currIdx, end)
+            # print(currIdx, nextEventIdx)
+            withCurrent =  value + dp(nextEventIdx, remainingEvents - 1)
+            
+            # print(currIdx, [start, end, value], withCurrent, withOutCurrent,  max(withCurrent, withOutCurrent))
+            cache[currIdx][remainingEvents] = max(withCurrent, withOutCurrent)
+            return cache[currIdx][remainingEvents]
+            
+
+        return dp(0, k)
