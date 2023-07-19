c6d2ee436327df364c694957e10e358538815d09
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2023-07-19 23:44:03 +0600

    Time: 1782 ms (5.02%) | Memory: 56.3 MB (12.86%) - LeetSync

---
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -0,0 +1,25 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        n = len(intervals)
+        
+        intervals.sort()
+        starts = [start for [start, end] in intervals]
+        dp = [1] * (n + 1)
+        
+        def getNextEvent(endTime: int)-> int:
+            i = bisect.bisect_left(starts, endTime)
+            if i == n:
+                return None
+            return i
+
+        print(intervals)
+        for i in range(n-1, -1, -1):
+            [start, end] = intervals[i]
+            j = getNextEvent(end)
+            if j == None:
+                dp[i] = max(dp[i], dp[i+1])
+            else:
+                dp[i] = max(dp[i], dp[j] + 1, dp[i+1])
+
+        print(dp)
+        return n - dp[0]
