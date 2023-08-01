e1b978e72948abe9dcc01f089922032d270f4f44
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2023-08-01 19:40:56 +0600

    Time: 825 ms (19.46%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-SELECT score, ((SELECT COUNT(DISTINCT Score) from Scores S2 where S1.score < S2.score) + 1) as 'Rank'
-FROM Scores S1
-GROUP BY S1.SCORE, S1.id
-ORDER BY S1.Score DESC
+SELECT score, DENSE_RANK() over (order by score desc) as 'Rank'
+FROM Scores
+ORDER BY Score DESC
