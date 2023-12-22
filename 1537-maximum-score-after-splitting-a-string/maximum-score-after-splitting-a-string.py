095d176637a0112c38f2dfc669b35f92c5a7ffd1
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2023-12-22 17:41:31 +0600

    Time: 43 ms (55.14%) | Memory: 17.4 MB (5.69%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -1,15 +1,8 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        zeros = 0 if s[0] == '1' else 1
-        ones = 0
-
-        for ch in s:
-            if ch == '1':
-                ones += 1
+        zeros = 1 if s[0] == '0' else 0
+        ones = sum([1 for ch in s[1:] if ch == '1'])
         
-        if s[0] == '1':
-            ones -= 1
-
         result = zeros + ones
 
         for i in range(1, len(s) - 1):
@@ -17,7 +10,7 @@ class Solution:
                 zeros += 1
             else:
                 ones -= 1
+            
             result = max(result, zeros + ones)
-
-        return result
         
+        return result
