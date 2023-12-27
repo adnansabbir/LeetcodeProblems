4eae9890e26e92f53d545a117b47a5605e42ebf2
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2023-12-27 08:31:47 +0600

    Time: 794 ms (98.20%) | Memory: 28.2 MB (9.18%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -1,23 +1,23 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        colors = colors + ('z' if colors[-1] != 'z' else 'a')
-        neededTime.append(1)
+        # Append a different character at the end to simplify calculations
+        colors += 'z' if colors[-1] != 'z' else 'a'
+        neededTime.append(0)  # Append a dummy time
 
-        totalTime = neededTime[0]
-        maxTime = neededTime[0]
-
-        result = 0
+        total_time = neededTime[0]  # Initialize total time with the first balloon's time
+        max_time = neededTime[0]    # Initialize max time with the first balloon's time
+        result = 0  # This will store the minimum time to remove the balloons
 
         for i in range(1, len(colors)):
-            if colors[i] == colors[i-1]:
-                totalTime += neededTime[i]
-
-                if neededTime[i] > maxTime:
-                    maxTime = neededTime[i]
+            if colors[i] == colors[i - 1]:
+                # Sum up the times and find the max time for consecutive balloons
+                total_time += neededTime[i]
+                max_time = max(max_time, neededTime[i])
             else:
-                result += totalTime - maxTime
-                totalTime = neededTime[i]
-                maxTime = neededTime[i]
+                # Calculate the cost for the previous group of same-colored balloons
+                result += total_time - max_time
+                # Reset for the next group of balloons
+                total_time = neededTime[i]
+                max_time = neededTime[i]
         
         return result
-        
