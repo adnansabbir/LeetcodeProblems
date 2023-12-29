0d3f1f244f2af27bff277947148bebd467e659df
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2023-12-29 21:45:08 +0600

    Time: 875 ms (27.92%) | Memory: 18.8 MB (6.19%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.py
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.py
@@ -1,29 +1,30 @@
+from typing import List
 from functools import lru_cache
 
 class Solution:
     def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
-        maxIdx = len(jobDifficulty) - 1
-        if len(jobDifficulty) < d:
+        if len(jobDifficulty) < d:  # Rule 1: If we have fewer jobs than days, scheduling is impossible.
             return -1
 
         @lru_cache(maxsize=None)
-        def minTime(idx: int, dLeft: int)-> int: 
-            if dLeft == 1:
+        def minDifficultyFrom(idx: int, daysRemaining: int) -> int:
+            """Calculate the minimum difficulty starting from a job index with given days remaining."""
+            if daysRemaining == 1:  # Base case: If only one day left, take the max difficulty from idx to end.
                 return max(jobDifficulty[idx:])
             
-            if idx > maxIdx:
+            if idx >= len(jobDifficulty):  # Base case: If index is out of bounds, no difficulty is added.
                 return 0
             
-            maxDifficulty = jobDifficulty[idx]
-            res = maxDifficulty + minTime(idx + 1, dLeft - 1)
+            maxDifficulty = jobDifficulty[idx]  # The difficulty of the current day starts with the current job.
+            result = maxDifficulty + minDifficultyFrom(idx + 1, daysRemaining - 1)  # Assume only this job is done today.
 
-            for i in range(idx + 1, maxIdx + 1 - (dLeft - 1)):
-                currMaxDifficulty = max(maxDifficulty, jobDifficulty[i])
-                currentRes = currMaxDifficulty + minTime(i + 1, dLeft - 1)
-                maxDifficulty = currMaxDifficulty
-                res = min(res, currentRes)
-                # print(f'Idx: {idx} i: {i} dL: {dLeft} cd: {currMaxDifficulty} res: {res}, taken:{jobDifficulty[idx: i+1]}')
-            return res
+            # Try doing more jobs today and see if it leads to a lower overall difficulty.
+            for i in range(idx + 1, len(jobDifficulty) - (daysRemaining - 1)):
+                maxDifficulty = max(maxDifficulty, jobDifficulty[i])  # Update max difficulty for the current day.
+                currentRes = maxDifficulty + minDifficultyFrom(i + 1, daysRemaining - 1)  # Recursively find the min difficulty for remaining days.
+                result = min(result, currentRes)  # Choose the minimum difficulty.
+            
+            return result
         
-        
-        return minTime(0, d)
+        # Start the recursion from the first job and with all days remaining.
+        return minDifficultyFrom(0, d)
