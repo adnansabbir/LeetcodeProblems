0418ea05d8c730c3fc90132989a3c5fb3ca0c728
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2023-12-31 18:50:17 +0600

    Time: 52 ms (85.71%) | Memory: 43.5 MB (50.00%) - LeetSync

---
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.ts
@@ -0,0 +1,23 @@
+function maxLengthBetweenEqualCharacters(s: string): number {
+        // Initialize an array to keep track of the first occurrence index for each character
+        const initialCharIndex: number[] = new Array(26).fill(-1);
+
+        // Initialize the maximum substring length as -1, assuming no repeats initially
+        let maxSubString: number = -1;
+
+        // Iterate over each character in the string
+        s.split('').forEach((char, i) => {
+            // Convert character to index (0 for 'a', 1 for 'b', etc.)
+            const charIndex: number = char.charCodeAt(0) - 'a'.charCodeAt(0);
+            if (initialCharIndex[charIndex] === -1) {
+                // If it's the first occurrence, record the index
+                initialCharIndex[charIndex] = i;
+            } else {
+                // If it's a repeat occurrence, calculate the gap and possibly update maxSubString
+                maxSubString = Math.max(maxSubString, i - initialCharIndex[charIndex] - 1);
+            }
+        });
+        
+        // Return the maximum substring length found
+        return maxSubString;
+    }
