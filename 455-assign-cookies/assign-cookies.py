00110ff940b32ba53e417deb3b13e14d9784b854
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-01-01 20:57:37 +0600

    Time: 170 ms (14.35%) | Memory: 18.8 MB (16.97%) - LeetSync

--- 455-assign-cookies/assign-cookies.py
+++ 455-assign-cookies/assign-cookies.py
@@ -1,12 +1,15 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        # Sort the greed factor and size of cookies in non-decreasing order
         g.sort()
         s.sort()
 
-        contentChildCount = 0
+        contentChildCount = 0  # Initialize the count of content children
+        # Loop until we run out of either children or cookies
         while g and s:
+            # If the smallest cookie can satisfy the least greedy child
             if s[0] >= g[0]:
-                contentChildCount += 1
-                g.pop(0)
-            s.pop(0)
-        return contentChildCount
+                contentChildCount += 1  # Satisfy the child
+                g.pop(0)  # Remove the child from the list
+            s.pop(0)  # Remove the cookie from the list
+        return contentChildCount  # Return the total number of content children
