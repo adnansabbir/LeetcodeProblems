932e22a225a06f03c669d2fad03fbfff9f3f7b90
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-01-03 06:20:53 +0600

    Time: 338 ms (5.01%) | Memory: 19.3 MB (17.25%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
@@ -1,10 +1,17 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        securityDeviceCount = [sum([1 for char in row if char == '1']) for row in bank]
-        securityDeviceCount = [count for count in securityDeviceCount if count > 0]
-        securityDeviceCount.append(0)
-        
         result = 0
-        for index in range(len(securityDeviceCount) - 1):
-            result += securityDeviceCount[index] * securityDeviceCount[index + 1]
+        prevRowDeviceCount = 0
+
+        for row in bank:
+            devicesInCurrentRow = 0
+            for cell in row:
+                devicesInCurrentRow += 1 if cell == '1' else 0
+            
+            if devicesInCurrentRow == 0:
+                continue
+            
+            result += prevRowDeviceCount * devicesInCurrentRow
+            prevRowDeviceCount = devicesInCurrentRow
+        
         return result
