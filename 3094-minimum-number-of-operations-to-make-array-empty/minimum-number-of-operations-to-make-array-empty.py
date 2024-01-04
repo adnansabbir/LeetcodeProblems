09a5792839db5153f3dc778a3de2852458f2adef
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-01-04 17:20:22 +0600

    Time: 552 ms (93.64%) | Memory: 31.6 MB (18.18%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
@@ -4,16 +4,11 @@ class Solution:
     def minOperations(self, nums: List[int]) -> int:
         freq = Counter(nums)
 
-        def countMinOperation(freq: int) -> int:
-            if freq < 2:
-                return -1
-            return math.ceil(freq / 3)
-
         result = 0
         for num in freq:
             if freq[num] == 1:
                 return -1
             
-            result += countMinOperation(freq[num])
+            result += math.ceil(freq[num] / 3)
         return result
         
