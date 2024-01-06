b331775538c2df589ce81cd5f6fe3f913195e833
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-01-06 14:42:19 +0600

    Time: 785 ms (33.03%) | Memory: 103.2 MB (18.77%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -1,11 +1,14 @@
 from functools import lru_cache
+from typing import List
 
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        startEndProfit = sorted([(s,e,p) for s,e,p in zip(startTime, endTime, profit)], key = lambda x: x[0])
+        # Combine the start time, end time, and profit into one list and sort it
+        startEndProfit = sorted([(s, e, p) for s, e, p in zip(startTime, endTime, profit)], key=lambda x: x[0])
         n = len(startTime)
 
-        def getNextIndexWithGreaterStartTime(time: int, start: int, end: int)-> int:
+        # Binary search helper function to find the next job index with a start time greater than the given time
+        def getNextIndexWithGreaterStartTime(time: int, start: int, end: int) -> int:
             while start <= end:
                 mid = (start + end) // 2
 
@@ -18,14 +21,17 @@ class Solution:
             
             return -1
 
-        @lru_cache(maxsize = None)
-        def countMaxProfit(currIndex: int)-> int:
+        # Recursive function to count the max profit using memoization
+        @lru_cache(maxsize=None)
+        def countMaxProfit(currIndex: int) -> int:
             if currIndex == n:
                 return 0
             
+            # Find the next job index that starts after the current job ends
             nextJobIdx = getNextIndexWithGreaterStartTime(startEndProfit[currIndex][1], currIndex + 1, n - 1)
             nextJobIdx = n if nextJobIdx == -1 else nextJobIdx
 
+            # Calculate the max profit by either taking the current job and adding the profit from the next available job or skipping the current job
             result = max(
                 startEndProfit[currIndex][2] + countMaxProfit(nextJobIdx),
                 countMaxProfit(currIndex + 1)
