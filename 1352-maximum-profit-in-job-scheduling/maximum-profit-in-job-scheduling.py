6c251c6a65effa7c5255ef2043143bce8e605176
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-01-06 14:36:28 +0600

    Time: 912 ms (14.80%) | Memory: 105.3 MB (17.36%) - LeetSync

---
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -0,0 +1,38 @@
+from functools import lru_cache
+
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        startEndProfit = sorted([(s,e,p) for s,e,p in zip(startTime, endTime, profit)], key = lambda x: x[0])
+        print(startEndProfit)
+        n = len(startTime)
+
+        def getNextIndexWithGreaterStartTime(time: int, start: int, end: int)-> int:
+            while start <= end:
+                mid = (start + end) // 2
+
+                if startEndProfit[mid][0] >= time and (mid == start or startEndProfit[mid-1][0] < time):
+                    return mid
+                elif startEndProfit[mid][0] >= time:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            
+            return -1
+
+        @lru_cache(maxsize = None)
+        def countMaxProfit(currIndex: int)-> int:
+            if currIndex == n:
+                return 0
+            
+            nextJobIdx = getNextIndexWithGreaterStartTime(startEndProfit[currIndex][1], currIndex + 1, n - 1)
+            nextJobIdx = n if nextJobIdx == -1 else nextJobIdx
+
+            print(f'For {currIndex} or end time {startEndProfit[currIndex][1]}, next is {nextJobIdx}')
+            result = max(
+                startEndProfit[currIndex][2] + countMaxProfit(nextJobIdx),
+                countMaxProfit(currIndex + 1)
+            )
+
+            return result
+
+        return countMaxProfit(0)
