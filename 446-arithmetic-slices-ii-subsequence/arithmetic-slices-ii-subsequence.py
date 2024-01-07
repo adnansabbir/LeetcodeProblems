d70f50b0d6b0944ef6cb824c84be2082bea94a81
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-01-07 15:18:30 +0600

    Time: 540 ms (66.55%) | Memory: 55.7 MB (54.68%) - LeetSync

---
+++ 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        sequences = {}
+        result = 0
+
+        for i, val in enumerate(nums):
+            for j in range(i):
+                diff = val - nums[j]
+                if i not in sequences or diff not in sequences[i]:
+                    if i not in sequences:
+                        sequences[i] = { diff: 0 }
+                    else:
+                        sequences[i][diff] = 0
+                
+                prevSeqCount = 0
+                if j in sequences and diff in sequences[j]:
+                    prevSeqCount = sequences[j][diff]
+
+                sequences[i][diff] += 1 + prevSeqCount
+
+                result += prevSeqCount
+        
+        # print(sequences, result)
+        return result
