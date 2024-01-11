81f9df046cb4a4c7e4c41a37d07d5af4b57d7854
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-01-11 11:26:37 +0600

    Time: 1563 ms (11.69%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,3 +1,3 @@
-select eu.unique_id, e.name
-from Employees as e
-left join EmployeeUNI as eu on e.id = eu.id
+select EmployeeUNI.unique_id, Employees.name
+from Employees
+left join EmployeeUNI on Employees.id = EmployeeUNI.id
