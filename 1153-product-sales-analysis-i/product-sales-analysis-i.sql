e7c30c6715b65f3e7c855d017dc4f2ac1b02b329
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-01-12 10:30:51 +0600

    Time: 2148 ms (44.19%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,3 +1,3 @@
-select Product.product_name as product_name, Sales.year as year, Sales.price as price
+select product_name, year, price
 from Sales
 left join Product on Sales.product_id = Product.product_id
