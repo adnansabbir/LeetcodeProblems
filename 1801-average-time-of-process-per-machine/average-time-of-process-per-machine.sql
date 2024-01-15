2d26b96318ebd42c9c1dba5752b34846c8a57183
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-01-15 08:45:33 +0600

    Time: 226 ms (94.15%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,10 +1,5 @@
-with processting_time as (
-    select a1.machine_id, a1.process_id, (a2.timestamp - a1.timestamp) as processing_time
-    from Activity a1
-    inner join Activity as a2 on (a1.machine_id = a2.machine_id and a1.process_id = a2.process_id and a2.activity_type = 'end')
-    where a1.activity_type = 'start'
-)
-
-select machine_id, round(avg(processing_time)::numeric, 3) as processing_time
-from processting_time
-group by machine_id
+select a1.machine_id, round(avg((a2.timestamp - a1.timestamp)::numeric), 3) as processing_time
+from Activity a1
+inner join Activity as a2 on (a1.machine_id = a2.machine_id and a1.process_id = a2.process_id and a2.activity_type = 'end')
+where a1.activity_type = 'start'
+group by a1.machine_id
