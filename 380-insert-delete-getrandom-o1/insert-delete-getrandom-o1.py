f822ff67b82c0f75ea1d1ebae4fe33c64c2a0ed4
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-01-16 08:51:38 +0600

    Time: 290 ms (81.25%) | Memory: 64.2 MB (39.43%) - LeetSync

---
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -0,0 +1,37 @@
+import random
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.uniqueData = {}
+        self.data = []
+
+    def insert(self, val: int) -> bool:
+        if val in self.uniqueData:
+            return False
+
+        self.data.append(val)
+        self.uniqueData[val] = len(self.data) - 1
+        return True
+        
+    def remove(self, val: int) -> bool:
+        if val not in self.uniqueData:
+            return False
+        
+        self.data[self.uniqueData[val]], self.data[-1] = self.data[-1], self.data[self.uniqueData[val]]
+        self.uniqueData[self.data[self.uniqueData[val]]] = self.uniqueData[val]
+        self.data.pop()
+        del self.uniqueData[val]
+        return True
+        
+    def getRandom(self) -> int:
+        # print(self.data)
+        randIndex = random.randint(0, len(self.data) - 1)
+        return self.data[randIndex]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
