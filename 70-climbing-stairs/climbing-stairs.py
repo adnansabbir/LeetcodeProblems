700238a3f381c7868cf43e99ebf4afc553b5e868
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-01-18 08:39:59 +0600

    Time: 29 ms (93.70%) | Memory: 17.2 MB (45.94%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,10 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [0 for _ in range(max(3, n + 1))]
-        dp[1] = 1
-        dp[2] = 2
+        if n < 3:
+            return n
+
+        dp = [1,2]
 
         for i in range(3, n + 1):
-            dp[i] = dp[i-1] + dp[i-2]
+            dp.append(dp[0] + dp[1])
+            dp.pop(0)
         
-        return dp[n]
+        return dp[-1]
