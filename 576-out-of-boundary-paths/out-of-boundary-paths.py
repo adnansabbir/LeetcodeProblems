1d8aff590a2871e2ccff221cc78239a63b067d0f
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-01-26 16:30:11 +0600

    Time: 101 ms (58.38%) | Memory: 26.8 MB (5.49%) - LeetSync

---
+++ 576-out-of-boundary-paths/out-of-boundary-paths.py
@@ -0,0 +1,17 @@
+from functools import lru_cache
+class Solution:
+    @lru_cache(maxsize=None)
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        if (startRow == m or startRow < 0) or (startColumn == n or startColumn < 0):
+            return 1
+        if maxMove == 0:
+            return 0
+        
+        return (
+            self.findPaths(m,n,maxMove-1, startRow-1, startColumn) 
+            + self.findPaths(m,n,maxMove-1, startRow+1, startColumn) 
+            + self.findPaths(m,n,maxMove-1, startRow, startColumn-1) 
+            + self.findPaths(m,n,maxMove-1, startRow, startColumn+1)
+        ) % (pow(10,9) + 7)
+            
+
