9b0cb73efee2948dfb34356d0af5febfe3305043
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-01-26 16:30:27 +0600

    Time: 59 ms (100.00%) | Memory: 22.8 MB (32.08%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/out-of-boundary-paths.py
@@ -1,17 +1,18 @@
 from functools import lru_cache
+
 class Solution:
-    @lru_cache(maxsize=None)
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
-        if (startRow == m or startRow < 0) or (startColumn == n or startColumn < 0):
-            return 1
-        if maxMove == 0:
-            return 0
         
-        return (
-            self.findPaths(m,n,maxMove-1, startRow-1, startColumn) 
-            + self.findPaths(m,n,maxMove-1, startRow+1, startColumn) 
-            + self.findPaths(m,n,maxMove-1, startRow, startColumn-1) 
-            + self.findPaths(m,n,maxMove-1, startRow, startColumn+1)
-        ) % (pow(10,9) + 7)
+        @lru_cache(maxsize=None)
+        def countPath(i: int, j: int, movesLeft: int) -> int:
+            if not (0 <= i < m and 0 <= j < n):
+                return 1
+            
+            if not movesLeft:
+                return 0
+            
+            return countPath(i+1, j, movesLeft - 1) + countPath(i-1, j, movesLeft - 1) + countPath(i, j + 1, movesLeft - 1) + countPath(i, j - 1, movesLeft - 1)
+        
+        return countPath(startRow, startColumn, maxMove) % (pow(10,9) + 7)
             
 
