83cfe69d27e9a974cc174972fd2f08f874711a9c
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-02-01 15:48:07 +0600

    Time: 743 ms (85.52%) | Memory: 30.7 MB (96.29%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -1,7 +1,6 @@
 class Solution:
     def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
         nums = sorted(nums)
-        # print(nums)
         result = [[] for _ in range(len(nums)//3)]
 
         for i in range(0, len(nums), 3):
@@ -12,6 +11,5 @@ class Solution:
                 result[resultIdx].append(nums[i])
                 result[resultIdx].append(nums[i+1])
                 result[resultIdx].append(nums[i+2])
-            # print(i, result)
         return result
         
