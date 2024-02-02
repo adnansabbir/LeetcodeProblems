49f580950ee5cac51a061ec99b187c0df37fdb76
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-02-02 20:08:22 +0600

    Time: 41 ms (28.88%) | Memory: 16.5 MB (63.35%) - LeetSync

--- 1212-sequential-digits/sequential-digits.py
+++ 1212-sequential-digits/sequential-digits.py
@@ -1,6 +1,9 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
         def getStartEndIncrement(digitCount: int)-> List[int]:
+            if digitCount > 9:
+                return [0,0,1]
+
             start = int(''.join([str(i + 1) for i in range(digitCount)]))
             end = int(''.join([str(9 - i) for i in range(digitCount)][::-1]))
             increment = int(''.join(['1' for i in range(digitCount)]))
@@ -12,7 +15,6 @@ class Solution:
         result = []
         while digitCount <= len(str(high)):
             start, end, increment = getStartEndIncrement(digitCount)
-            # print(digitCount, [start, end, increment])
             for num in range(start, end + 1, increment):
                 if low <= num <= high:
                     result.append(num)
