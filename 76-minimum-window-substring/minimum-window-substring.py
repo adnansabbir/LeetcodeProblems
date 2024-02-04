6109f2daaf96e16d432ef46895afd2a8c768feb3
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-02-04 16:10:13 +0600

    Time: 89 ms (81.66%) | Memory: 17.2 MB (76.65%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        tFreq = Counter(t)
+        sFreq = {}
+        charLeftToBeTaken = len(t)
+
+        l, r = 0, 0
+        result = ''
+        print('TFreq\t', tFreq)
+        
+        while r < len(s):
+            rChar = s[r]
+            sFreq[rChar] = sFreq.get(rChar, 0) + 1
+
+            if rChar in tFreq:
+                if sFreq[rChar] <= tFreq[rChar]:
+                    charLeftToBeTaken -= 1
+            while charLeftToBeTaken == 0:
+                lChar = s[l]
+                
+                sFreq[lChar] -= 1
+                if lChar in tFreq:
+                    if sFreq[lChar] < tFreq[lChar]:
+                        if not result:
+                            result = s[l:r+1]
+                        elif len(result) > (r - l):
+                            result = s[l:r+1]
+                        charLeftToBeTaken += 1
+                l += 1
+            r += 1
+        return result
