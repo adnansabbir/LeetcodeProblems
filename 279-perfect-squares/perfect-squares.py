726530d9453af694c931392b53b2b1015434a294
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-02-08 22:19:55 +0600

    Time: 3992 ms (19.25%) | Memory: 16.9 MB (74.60%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -1,19 +1,17 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        perfectSquares = [i*i for i in range(1, int(sqrt(n)) + 1) if i*i <= n]
-        # print('Squares', perfectSquares)
         dp = [0 for _ in range(n + 1)]
 
-        for square in perfectSquares:
-            # print('\n\nsquare\t', square)
+        for num in range(1, int(sqrt(n)) + 1):
+            if num * num > n:
+                continue
+
+            square = num * num
             for targetIdx in range(1, n + 1):
                 if targetIdx % square == 0:
                     dp[targetIdx] = targetIdx // square
                 else:
                     dp[targetIdx] = min((targetIdx // square) + dp[targetIdx%square], dp[targetIdx])
-            # result = min(result, dp[-1])
-            # print('After\t', dp, result)
 
-        # print(dp)
         return dp[-1]
         
