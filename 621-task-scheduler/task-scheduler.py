27be6780ee40bbecfcca3033e4688c9bb427f562
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-03-20 02:57:11 +0600

    Time: 351 ms (91.07%) | Memory: 17.1 MB (58.17%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,13 +1,16 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        freq = [0] * 26
-        for task in tasks:
-            freq[ord(task) - ord('A')] += 1
-        freq.sort()
-        chunk = freq[25] - 1
-        idle = chunk * n
+        task_counts = Counter(tasks)
+        max_frequency = max(task_counts.values())
+        max_frequency_tasks = sum(1 for task, count in task_counts.items() if count == max_frequency)
+        
+        # Calculate the minimum time directly
+        part_count = max_frequency - 1
+        part_length = n - (max_frequency_tasks - 1)
+        empty_slots = part_count * part_length
+        available_tasks = len(tasks) - max_frequency * max_frequency_tasks
+        idles = max(0, empty_slots - available_tasks)
 
-        for i in range(24, -1, -1):
-            idle -= min(chunk, freq[i])
+        return len(tasks) + idles
 
-        return len(tasks) + idle if idle >= 0 else len(tasks)
+        
