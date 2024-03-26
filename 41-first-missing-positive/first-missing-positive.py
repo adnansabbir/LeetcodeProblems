6ab7f5f2003cd1979a7deb24b835284a3c41294f
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-03-27 02:03:11 +0600

    Time: 315 ms (18.91%) | Memory: 30.4 MB (55.56%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -2,7 +2,6 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         pointer = 0
         while pointer < len(nums):
-            # print("Pointer -> ", pointer)
             if nums[pointer] <= 0 or nums[pointer] > len(nums):
                 nums[pointer] = -1
                 pointer += 1
@@ -10,14 +9,11 @@ class Solution:
                 pointer += 1
             else:
                 left, right = pointer, nums[pointer] - 1
-                # print(f'Swap {left}:{nums[left]} \twith {right}:{nums[right]}')
                 if right + 1 == nums[right]:
                     nums[left] = -1
                     pointer += 1
                     continue
                 nums[left], nums[right] = nums[right], nums[left]
-
-            # print(pointer, nums)
         
         for i, num in enumerate(nums):
             if num == -1:
