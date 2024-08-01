c24735e0085c7358b0b5f20b5e0a8670ad2f0971
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-08-01 22:12:44 +0400

    Time: 48 ms (47.08%) | Memory: 16.5 MB (32.17%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.py
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.py
@@ -1,4 +1,8 @@
 class Solution:
     def countSeniors(self, details: List[str]) -> int:
-        return sum([1 for passenger in details if int(passenger[11:13]) > 60])
+        result = 0
+        for i in range(len(details)):
+            result += 1 if int(details[i][11:13]) > 60 else 0
+        
+        return result
         
