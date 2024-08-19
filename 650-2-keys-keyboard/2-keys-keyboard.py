d4a6088b2d6cd77c8e39df4e5f56656a2aeac891
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-08-19 18:45:20 +0400

    Time: 422 ms (14.80%) | Memory: 16.6 MB (36.56%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -1,20 +1,16 @@
 class Solution:
     def minSteps(self, n: int) -> int:
         def get_min_count(on_notobook: int, on_clipboard: int, copied = False)-> int:
-            # print(on_notobook, on_clipboard, copied)
             if on_notobook == n:
                 return 0
             elif on_notobook > n or on_clipboard > n:
                 return 9999
             
             if not on_clipboard:
-                # print('Copy')
                 return  get_min_count(on_notobook, on_notobook, True) + 1
             elif copied:
-                # print('Pasting')
                 return  get_min_count(on_notobook + on_clipboard, on_clipboard, False) + 1
             else:
-                # print('Copy and Paste')
                 return min(
                     get_min_count(on_notobook, on_notobook, True),
                     get_min_count(on_notobook + on_clipboard, on_clipboard, False)
