7cd4966f5111c9a90fd8433b7ed9593fa59e6c46
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-09-28 03:40:06 +0400

    Time: 35 ms (82.42%) | Memory: 16.6 MB (11.64%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,36 +1,17 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        if not n:
-            return nums1
+        # Set pointers for nums1 and nums2
+        p1, p2, p = m - 1, n - 1, m + n - 1
 
-        if not m:
-            for i in range(n):
-                nums1[i] = nums2[i]
-            return nums1
-
-        left, right = m - 1, len(nums1) - 1
-        while left > -1:
-            nums1[left], nums1[right] = 0, nums1[left]
-            right -= 1
-            left -= 1
-        
-        p2, p1 = 0, n
-        for i in range(m + n):
-            if not (p1 >= len(nums1) or p2 >= len(nums2)):
-                if nums1[p1] <= nums2[p2]:
-                    nums1[i] = nums1[p1]
-                    p1 += 1
-                else:
-                    nums1[i] = nums2[p2]
-                    p2 += 1
+        # Traverse both arrays from the end
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
             else:
-                if p1 < len(nums1):
-                    nums1[i] = nums1[p1]
-                    p1 += 1
-                else:
-                    nums1[i] = nums2[p2]
-                    p2 += 1
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
 
-        # print(p1, p2, i)
-
-        return nums1
+        # If there are remaining elements in nums2, copy them
+        nums1[:p2 + 1] = nums2[:p2 + 1]
