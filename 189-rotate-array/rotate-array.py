17c68a7a1bba5e20e0d2325e221ea805d697f05f
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-09-28 05:05:15 +0400

    Time: 173 ms (20.57%) | Memory: 28 MB (31.08%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k = k % len(nums)
+        if not k:
+            return nums
+
+        start, pos, num = 0, 0, nums[0]
+
+        for i in range(len(nums)):
+            next_pos = (pos + k) % len(nums)
+            nums[next_pos], num, pos = num, nums[next_pos], next_pos
+            # print(nums, num, pos)
+            if pos == start:
+                pos += 1
+                start += 1
+                num = nums[pos]
+        return nums
+
+
