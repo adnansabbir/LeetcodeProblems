edcd1b87dc9ed85af7399f63a6c0021ffa99cc4b
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-10-01 19:58:26 +0400

    Time: 495 ms (66.59%) | Memory: 30.5 MB (57.83%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -5,8 +5,7 @@ class Solution:
         if freq[0] % 2 != 0:
             return False
 
-        # print(freq)
-        for i in range(1, k):
+        for i in range(1, k//2 + 1):
             if freq[i] != freq[k - i]:
                 return False
         return True
