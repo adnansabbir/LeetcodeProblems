6802aaed0de4f46fc66c6dc893101722b6e210b6
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-10-10 21:31:11 +0400

    Time: 431 ms (11.09%) | Memory: 26.3 MB (17.02%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.py
@@ -0,0 +1,16 @@
+from bisect import bisect_left
+
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        m_stack = []
+        result = 0
+        for i, num in enumerate(nums):
+            if not m_stack or -num > m_stack[-1][0]:
+                m_stack.append((-num, i))
+                continue
+            
+            pos = bisect_left(m_stack, -num, key = lambda x: x[0])
+            result = max(result, i - m_stack[pos][1])
+            # print(i, num, m_stack, pos)
+
+        return result
