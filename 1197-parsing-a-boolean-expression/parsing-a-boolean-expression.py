adda48de7ac26f40d3572201c412dac7bed44a2f
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-10-20 13:40:25 +0400

    Time: 15 ms (100.00%) | Memory: 16.8 MB (19.76%) - LeetSync

---
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.py
@@ -0,0 +1,38 @@
+class Solution:
+    def evaluate(self, logic: str, expr: List[str])-> bool:
+        # print('Evaluating', logic, expr)
+        if logic == '!':
+            return not expr[0]
+        elif logic == '&':
+            return all(expr)
+        elif logic == '|':
+            return any(expr)
+
+    def parseBoolExpr(self, expression: str) -> bool:
+        lastLogic = []
+        stack = []
+        current_expr = []
+        
+        for char in expression:
+            if char in ['!', '&', '|']:
+                lastLogic.append(char)
+            elif char in ['(', 't', 'f']:
+                if char == 't':
+                    stack.append(True)
+                elif char == 'f':
+                    stack.append(False)
+                else:
+                    stack.append(char)
+
+            elif char == ')':
+                ce_char = stack.pop()
+                while ce_char != '(':
+                    current_expr.append(ce_char)
+                    ce_char = stack.pop()
+                stack.append(self.evaluate(lastLogic.pop(), current_expr))
+                current_expr = []
+            
+            # print(char, lastLogic, stack)
+
+        return stack[0]
+        
