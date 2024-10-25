e1d24b00f7ae5ac0b4293a4363b7a7be2b3837ba
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-10-25 20:29:53 +0400

    Time: 55 ms (83.79%) | Memory: 30.4 MB (76.21%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -3,25 +3,20 @@ class Solution:
         trie = {}
         result = []
         folder.sort(key=lambda x: len(x))
-        # print(folder)
         for directory in folder:
             trie_pointer = trie
             take_dir = True
             for dir_name in directory.split('/'):
-                # print('\t before', dir_name, trie_pointer, trie_pointer.get('end'))
                 if trie_pointer.get('end'):
                     take_dir = False
                     break
                 elif dir_name not in trie_pointer:
                     trie_pointer[dir_name] = {}
                 trie_pointer = trie_pointer.get(dir_name)
-                    
-                # print('\t after', dir_name, trie_pointer)
             
             if take_dir:
                 trie_pointer['end'] = True
                 result.append(directory)
-            # print(trie, result)
         
         return result
 
