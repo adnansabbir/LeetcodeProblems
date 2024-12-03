27ce166c6376050dea900ac59cb35b9f09e32ecb
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-12-03 21:28:49 +0400

    Time: 43 ms (86.30%) | Memory: 49 MB (40.48%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
@@ -1,11 +1,12 @@
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
         result = []
+        prev_point = 0
+        s_pointer = 0
+        for next_point in spaces:
+            result.append(s[prev_point: next_point])
+            prev_point = next_point
+            s_pointer += 1
 
-        for i in range(len(s)):
-            if spaces and i == spaces[0]:
-                result.append(' ')
-                spaces.pop(0)
-            
-            result.append(s[i])
-        return ''.join(result)
+        result.append(s[prev_point:])
+        return ' '.join(result)
