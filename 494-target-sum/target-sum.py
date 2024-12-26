6bad7da55c892eee6c568f09e0a02250204ee474
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2024-12-26 21:18:26 +0400

    Time: 142 ms (69.99%) | Memory: 45.2 MB (5.57%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -4,16 +4,16 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         cache = {}
 
+        @lru_cache(maxsize=None)
         def exp_count(i, total):
-            key = f'{i}_{total}'
-            if key in cache:
-                return cache[key]
+            # key = f'{i}_{total}'
+            # if key in cache:
+            #     return cache[key]
 
             if i == len(nums):
                 return 1 if total == target else 0
             
-            cache[key] = exp_count(i + 1, total + nums[i]) + exp_count(i + 1, total - nums[i])
-            return cache[key]
+            return exp_count(i + 1, total + nums[i]) + exp_count(i + 1, total - nums[i])
 
         return exp_count(0, 0)
         
