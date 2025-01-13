6c508f7624bede6cae0e57c1ea48fa19046256bd
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-01-13 21:57:54 +0400

    Time: 132 ms (66.98%) | Memory: 18.8 MB (32.70%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        freq = Counter(s)
+        
+        result = 0
+        for count in freq.values():
+            if count <= 2 == 0:
+                result += 2
+            elif count % 2 == 0:
+                result += 2
+            else:
+                result += 1
+        return result
+
+        
