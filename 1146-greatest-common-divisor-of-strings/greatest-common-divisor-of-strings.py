b5d62707a7b41f074bd32c7ce7f4763502443d26
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-06-26 03:31:16 +0400

    Time: 3 ms (25.70%) | Memory: 17.9 MB (46.70%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        str1_len, str2_len = len(str1), len(str2)
+
+        def is_string_repetative(main_str, sub_str):
+            if len(main_str) % len(sub_str) == 0:
+                return sub_str * (len(main_str) // len(sub_str)) == main_str
+            return False
+        
+        gcd_str = str2
+        for i in range(str2_len, 0, -1):
+            if is_string_repetative(str2, str2[:i]) and is_string_repetative(str1, str2[:i]):
+                return str2[:i]
+        return ''
+        
