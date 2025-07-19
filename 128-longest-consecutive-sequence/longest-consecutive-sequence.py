004e49b442d9d30f74aca93bc90f14bdaa83acc3
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-07-19 21:20:13 +0400

    Time: 52 ms (52.68%) | Memory: 34.2 MB (52.58%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,40 +1,24 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        nsm = {}
-        longest_size = 1 if nums else 0
-
-        def merge(start, end):
-            starting_num = start.get("start")
-            ending_num = end.get("end")
-
-            new_size = start.get('size') + end.get('size')
-
-            # Setting start node
-            nsm[starting_num]['size'] = new_size
-            nsm[starting_num]['end'] = ending_num
-            # Setting End Node
-            nsm[ending_num]['size'] = new_size
-            nsm[ending_num]['start'] = starting_num
-
-            return new_size
-
+        nums_set = set(nums)
+        result = 0
+        
         for num in nums:
-            if num not in nsm:
-                nsm[num] = {"size": 1, "start": num, "end": num}
-            else:
+            if num not in nums_set:
                 continue
-
-            snsm = nsm.get(num - 1, None)
-            ensm = nsm.get(num + 1, None)
-
-            # if boths are there merge them
-            if snsm and ensm:
-                merge(snsm, nsm[num])
-                longest_size = max(longest_size, merge(nsm[num], ensm))
-            # if -1 is there, append to it
-            elif snsm:
-                longest_size = max(longest_size, merge(snsm, nsm[num]))
-            elif ensm:
-                longest_size = max(longest_size, merge(nsm[num], ensm))
-
-        return longest_size
+            
+            next_num = num
+            seq_size = 0
+            while next_num in nums_set:
+                nums_set.remove(next_num)
+                seq_size += 1
+                next_num += 1
+            
+            prev_num = num - 1
+            while prev_num in nums_set:
+                nums_set.remove(prev_num)
+                seq_size += 1
+                prev_num -= 1
+            result = max(result, seq_size)
+        
+        return result
