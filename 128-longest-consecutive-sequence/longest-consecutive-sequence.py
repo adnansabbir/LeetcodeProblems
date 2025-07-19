d7980fb04a788480f4eec2ed36ca181e79063440
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-07-19 21:07:55 +0400

    Time: 260 ms (5.01%) | Memory: 56.7 MB (5.05%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -0,0 +1,50 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nsm = {}
+        longest_size = 1 if nums else 0
+
+        def merge(start, end):
+            # print('Before')
+            # print('start', start)
+            # print('end', end)
+            starting_num = start.get("start")
+            ending_num = end.get("end")
+
+            new_size = start.get('size') + end.get('size')
+
+            # Setting start node
+            nsm[starting_num]['size'] = new_size
+            nsm[starting_num]['end'] = ending_num
+            # Setting End Node
+            nsm[ending_num]['size'] = new_size
+            nsm[ending_num]['start'] = starting_num
+
+            # print('\nAfter')
+            # print('start', start)
+            # print('end', end)
+
+            # print('New Size', new_size)
+            return new_size
+
+        for num in nums:
+            # print('\n\n', 'Working on ', num)
+            # not in seq than add it
+            if num not in nsm:
+                nsm[num] = {"size": 1, "start": num, "end": num}
+            else:
+                continue
+
+            snsm = nsm.get(num - 1, None)
+            ensm = nsm.get(num + 1, None)
+
+            # if boths are there merge them
+            if snsm and ensm:
+                merge(snsm, nsm[num])
+                longest_size = max(longest_size, merge(nsm[num], ensm))
+            # if -1 is there, append to it
+            elif snsm:
+                longest_size = max(longest_size, merge(snsm, nsm[num]))
+            elif ensm:
+                longest_size = max(longest_size, merge(nsm[num], ensm))
+
+        return longest_size
