9abf051e415fec08608088dbf1c0509a59988e87
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-07-22 22:02:58 +0400

    Time: 0 ms (100.00%) | Memory: 23.3 MB (81.26%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,22 +8,42 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head:
-            return None
-        q = []
-        temp_head = head
-        while temp_head:
-            q.append(temp_head)
-            temp_head = temp_head.next
-        
-        result = ListNode()
-        temp_head = result
-        is_start = True
-        while q:
-            temp_head.next = q.pop(0) if is_start else q.pop()
-            is_start = not is_start
-            temp_head = temp_head.next
-        temp_head.next = None
+        if not head or not head.next or not head.next.next:
+            return head
 
-        return result.next
+        def reverse_linked_list(head):
+            if not head or not head.next:
+                return head
+            
+            first, second = None, head
+
+            while second:
+                first, second.next, second = second, first, second.next
+            return first
+
+        slow, fast = head, head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        first_half = head
+        second_half = reverse_linked_list(slow.next)
+        slow.next = None
+        
+        temp = ListNode()
+        while first_half or second_half:
+            if first_half:
+                temp.next = first_half
+                temp = temp.next
+                first_half = first_half.next
+            if second_half:
+                temp.next = second_half
+                temp = temp.next
+                second_half = second_half.next
+        
+        return head
+            
+                
+        
         
