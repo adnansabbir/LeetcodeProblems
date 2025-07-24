31ccf7aef8dcf6e94476260163874777ecbcbc80
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-07-24 06:22:32 +0400

    Time: 0 ms (100.00%) | Memory: 18 MB (80.65%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,14 +1,5 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        # [4,5,1,2,3]
-        # # if mid-1 is > and mid+1 is <, this is the target
-        
-        # [5,1,2,3,4]
-        # # if left is > mid, result is in between
-
-        # [3,4,5,1,2]
-        # # if right < mid target is between
-
         left, right = 0, len(nums) - 1
 
         while left <= right:
