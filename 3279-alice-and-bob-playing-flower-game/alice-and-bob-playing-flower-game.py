af6096fcbdd7385008834cd08f547a5fd074fedd
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-08-29 08:47:27 +0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (43.75%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
@@ -1,4 +1,8 @@
 class Solution:
     def flowerGame(self, n: int, m: int) -> int:
+        # THe problem statement is confusing
+        # Here we need to find out 2 values x = Flower count in Lane 1
+        # and y = Flower count in lane 2
+        # x and y has to be such that Alice wins at the end of the game
         return (n*m) // 2
         
