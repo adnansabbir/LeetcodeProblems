06a54f912e9c2a7e8793d7c49fcc6c24e3459fa9
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-08-31 13:11:36 +0400

    Time: 2531 ms (21.03%) | Memory: 18.1 MB (39.52%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.py
+++ 37-sudoku-solver/sudoku-solver.py
@@ -23,23 +23,7 @@ class Solution:
                 cols[col].add(board[row][col])
                 triplets[getTripletPos(row, col)].add(board[row][col])
         
-        # print('Rows')
-        # for row in rows:
-        #     print(row)
-        # print('Cols')
-        # for row in cols:
-        #     print(row)
-        # print('Trip')
-        # for row in triplets:
-        #     print(row)
         
-        def is_valid(x, y, val):
-            return not (val in rows[x] or val in cols[y] or val in triplets[getTripletPos(x,y)])
-
-        # print(is_valid(0,2, '5'))
-        # print(is_valid(1,1, '5'))
-        # print(is_valid(2,0,'5'))
-        # print(is_valid(0,2, '4'))
         possible_nums = set([str(num) for num in range(1, 10)])
         def solve(idx = 0):
             if idx == len(empty_pos):
@@ -49,9 +33,6 @@ class Solution:
             valid_nums = list(possible_nums - invalid_nums)
             for num in valid_nums:
                 num_str = str(num)
-                
-                # if not is_valid(row, col, str(num)):
-                #     continue
                 
                 rows[row].add(num)
                 cols[col].add(num)
