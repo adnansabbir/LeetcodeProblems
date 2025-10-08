4aebca45fcfdf1b1708f361667e769b2dffbe70c
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-10-08 10:56:29 +0600

    Time: 154 ms (94.96%) | Memory: 40.3 MB (99.15%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
@@ -5,18 +5,10 @@ class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
         potions.sort()
         m = len(potions)
-        cache = {}
-        # print(potions)
 
         for idx, spell in enumerate(spells):
-            # print(spell, cache)
-            if spell in cache:
-                spells[idx] = cache[spell]
-                continue
-
             num_gt = math.ceil(success/spell)
             spells[idx] = m - bisect_left(potions, num_gt)
-            cache[spell] = spells[idx]
         return spells
 
         
