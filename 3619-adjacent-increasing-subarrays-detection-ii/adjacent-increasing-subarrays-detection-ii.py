e663d3a004ca31da04fd4ca633253245df9d06e1
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-10-15 11:48:33 +0600

    Time: 1355 ms (71.61%) | Memory: 45.8 MB (39.35%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.py
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.py
@@ -6,17 +6,15 @@ class Solution:
         result = 1
         nums.append(None)
         # streak, streak_no, end_index
-        last_streak = (1, 0, 0)
+        last_streak = 1
         streak = 1
         for i in range(1, len(nums)):
             if nums[i] != None and nums[i] > nums[i-1]:
                 streak += 1
             else:
                 # compare with last streak
-                result = max(result, min(last_streak[0], streak), streak//2)
-
-                last_streak = (streak, last_streak[1] + 1, i-1)
+                result = max(result, min(last_streak, streak), streak//2)
+                last_streak = streak
                 streak = 1
-                # print(last_streak)
         return result
             
