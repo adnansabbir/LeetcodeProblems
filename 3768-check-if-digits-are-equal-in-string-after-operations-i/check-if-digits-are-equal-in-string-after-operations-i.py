703d1b6fd7458aa2ee4c5150c0246f36230841e3
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-10-23 11:00:04 +0600

    Time: 25 ms (83.85%) | Memory: 17.7 MB (71.17%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.py
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.py
@@ -7,6 +7,5 @@ class Solution:
             for i in range(1, n):
                 nums[i-1] = (nums[i-1] + nums[i]) %10
             n-=1
-            # print(nums)
 
         return nums[0] == nums[1]
