cf0aa7172fe11bb83b12fa020cca4fc22481aeb0
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-10-28 08:51:19 +0400

    Time: 52 ms (41.49%) | Memory: 17.7 MB (54.26%) - LeetSync

---
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countValidSelections(self, nums: List[int]) -> int:
+        result = 0
+        right = sum(nums)
+        left = 0
+
+        for num in nums:
+            if num == 0:
+                if left == right:
+                    result += 2
+                elif abs(left - right) == 1:
+                    result += 1
+            right -= num
+            # print(left, num, right)
+            left += num
+        
+        return result
+        
