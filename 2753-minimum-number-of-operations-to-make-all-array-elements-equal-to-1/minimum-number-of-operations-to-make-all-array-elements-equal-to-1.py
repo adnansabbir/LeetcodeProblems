9bc6c3c82e942489dcc0d77fa330a7d87ffe3a8b
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-11-12 19:35:23 +0400

    Time: 4 ms (24.71%) | Memory: 18.1 MB (12.94%) - LeetSync

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.py
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.py
@@ -18,44 +18,40 @@ class Solution:
         if even_count == len(nums):
             return -1
 
-        # TODO: for each number we need to check which one gives the fastest 1
-        def score_to_reduce_to_1(idx)-> int:
+        # for each number we need to check which one gives the fastest 1
+        def score_to_reduce_to_1(idx, curr_num)-> int:
             result = float('-inf')
 
             if idx == 0:
                 return result
             
-            if nums[idx] == nums[idx-1]:
+            if curr_num == nums[idx-1]:
                 return result
 
-            num_prev, num = nums[idx-1], nums[idx]
-            gcd = math.gcd(num_prev, num)
+            gcd = math.gcd(nums[idx-1], curr_num)
             
             if gcd == 1:
                 return 1
 
             left_res, right_res = float('-inf'), float('-inf')
             if gcd != nums[idx-1]:
-                nums[idx-1] = gcd
-                res_prev = score_to_reduce_to_1(idx-1)
+                res_prev = score_to_reduce_to_1(idx-1, gcd)
 
                 if res_prev != float('-inf'):
                     left_res = res_prev + 1
-            if gcd != nums[idx]:
-                nums[idx] = gcd
-                res_curr = score_to_reduce_to_1(idx)
+            if gcd != curr_num:
+                res_curr = score_to_reduce_to_1(idx, gcd)
 
                 if res_curr != float('-inf'):
                     right_res = res_curr + 1
 
-            nums[idx-1], nums[idx] = num_prev, num
             if left_res != float('-inf') and right_res != float('-inf'):
                 return min(left_res, right_res)
             return max(left_res, right_res)
         
         min_op_for_first_one = float('inf')
         for i in range(1, len(nums)):
-            if (op:= score_to_reduce_to_1(i)) != float('-inf'):
+            if (op:= score_to_reduce_to_1(i, nums[i])) != float('-inf'):
                 min_op_for_first_one = min(min_op_for_first_one, op)
 
         if min_op_for_first_one != float('inf'):
