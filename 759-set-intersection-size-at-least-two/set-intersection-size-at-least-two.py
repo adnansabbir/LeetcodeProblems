d7952fdc4cad3ed5c8f2e4885075fcb3ba50f4d4
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2025-11-20 09:36:02 +0400

    Time: 14 ms (33.79%) | Memory: 19.3 MB (6.90%) - LeetSync

--- 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.py
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.py
@@ -3,18 +3,26 @@ class Solution:
         intervals.sort(key= lambda x: (x[1], x[1]-x[0], x[1]))
 
         result = []
+        count = 0
         for interval in intervals:
             if not result or result[-1] < interval[0]:
                 result.append(interval[-1] - 1)
                 result.append(interval[-1])
+                count += 2
             elif interval[0] < result[-1]:
                 if interval[0] <= result[-2]:
                     continue
                 elif result[-2] > interval[0] <=  result[-1]:
                     result.append(interval[1])
+                    count += 1
                 else:
-                    result.append(interval[1])    
+                    result.append(interval[1])
+                    count += 1    
             elif interval[0] == result[-1]:
                 result.append(interval[1])
-        return len(result)
+                count += 1
+            
+            while len(result) > 2:
+                result.pop(0)
+        return count
         
