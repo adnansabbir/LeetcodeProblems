1d719f26117a9776ed1ef5eacf9312d733b535d9
Adnan Sabbir <18300133+adnansabbir@users.noreply.github.com>
2025-12-30 14:43:16 +0400

    Time: 4 ms (12.12%) | Memory: 17.6 MB (82.42%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.py
+++ 870-magic-squares-in-grid/magic-squares-in-grid.py
@@ -1,82 +1,37 @@
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        if len(grid) < 3 or len(grid[0]) < 3:
-            return 0
+        def is_magic_grid(x1:int, y1:int, x2: int, y2:int)-> bool:
+            uniq = set()
+            r_sums, c_sums, diag_sum = [0,0,0], [0,0,0], [0,0]
+            for i in range(x1, x2):
+                for j in range(y1, y2):
+                    # uniq check
+                    if grid[i][j] in uniq or grid[i][j] > 9 or grid[i][j] < 1:
+                        return False
+                    else:
+                        uniq.add(grid[i][j])
 
-        nums_in_square = []
+                    r,c = i - x1, j - y1
+                    # row col sum
+                    r_sums[r] += grid[i][j]
+                    c_sums[c] += grid[i][j]
 
-        def print_magic():
-            for row in nums_in_square:
-                print(row)
-            print('\n')
+                    # tl to rb diag sum
+                    if r == c:
+                        diag_sum[0] += grid[i][j]
 
-        def is_magic_square(grid):
-            # Check if grid is 3x3
-            if len(grid) != 3 or any(len(row) != 3 for row in grid):
-                return False
+                    # tr to lb diag sum
+                    if (r == 0 and c == 2) or (r == 1 and c == 1) or (r == 2 and c == 0):
+                        diag_sum[1] += grid[i][j]
 
-            # Check if all numbers from 1 to 9 are present
-            numbers = set(num for row in grid for num in row)
-            if numbers != set(range(1, 10)):
-                return False
+            return r_sums[0] == r_sums[1] and r_sums[0] == r_sums[2] and r_sums[0] == c_sums[0] and r_sums[0] == c_sums[1] and r_sums[0] == c_sums[2] and r_sums[0] == diag_sum[0] and r_sums[0] == diag_sum[1]
 
-            # Calculate the sum of the first row to compare with others
-            magic_sum = sum(grid[0])
-
-            # Check sum of each row
-            for row in grid:
-                if sum(row) != magic_sum:
-                    return False
-
-            # Check sum of each column
-            for col in range(3):
-                if sum(grid[row][col] for row in range(3)) != magic_sum:
-                    return False
-
-            # Check sum of the diagonals
-            if sum(grid[i][i] for i in range(3)) != magic_sum:
-                return False
-
-            if sum(grid[i][2 - i] for i in range(3)) != magic_sum:
-                return False
-
-            return True
-
+        m, n = len(grid), len(grid[0])
         result = 0
-        for i in range(3):
-            if len(nums_in_square) <= i:
-                nums_in_square.append([])
-            for j in range(3):
-                nums_in_square[i].append(grid[i][j])
-        
-        result += is_magic_square(nums_in_square)
-
-        def update_magic_square(nums: list[int], right = True):
-            print(nums, right)
-            if right:
-                for row in nums_in_square:
-                    row.pop(0)
-                    row.append(nums.pop(0))
-            else:
-                nums_in_square.pop(0)
-                nums_in_square.append(nums)
-
-        for i in range(2, len(grid)):
-            # from next iteration
-            if i > 2:
-                print('Going Down')
-                update_magic_square([grid[i][0], grid[i][1], grid[i][2]], False)
-                result += is_magic_square(nums_in_square)
-
-            temp_num_in_square = [row.copy() for row in nums_in_square]
-            for j in range(3, len(grid[0])):
-                print('Going right')
-                update_magic_square([grid[i-2][j], grid[i-1][j], grid[i][j]])
-                result += is_magic_square(nums_in_square)
-            nums_in_square = temp_num_in_square
-
+        for i in range(m-2):
+            for j in range(n - 2):
+                if is_magic_grid(i,j, i+3, j+3):
+                    result += 1
         
         return result
-
-
         
