418bf0f3c1f5c858be7dce84a8986f612fba33f5
Adnan Sabbir <18300133+adnansabbir@users.noreply.github.com>
2026-02-10 07:43:31 +0400

    Time: 318 ms (87.23%) | Memory: 19.6 MB (57.02%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.py
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.py
@@ -5,6 +5,9 @@ class Solution:
             evens = set()
             odds = set()
 
+            if result >= len(nums) - i:
+                return result
+
             for j in range(i, len(nums)):
                 if nums[j] % 2 == 0:
                     evens.add(nums[j])
